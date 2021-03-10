06fbea0095385cc5c16d6daf0fda5b5049160493
HQCfly <2249629867@qq.com>
2021-03-10 23:59:03 +0800

    Add maxRectangle

---
+++ src/main/java/com/twiss/arraypractice/MaximalRectangle.java
@@ -0,0 +1,63 @@
+package com.twiss.arraypractice;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/3/10 11:37 下午
+ */
+public class MaximalRectangle {
+
+    public static int maxArea(int[][] numbers) {
+        int m = numbers.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = numbers[0].length;
+        int[][] left = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (numbers[i][j] == 1) {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+        int ret = 0;
+        for (int j = 0; j < n; j++) {
+            int[] up = new int[m];
+            int[] down = new int[m];
+            Stack<Integer> stack = new Stack<Integer>();
+            for (int i = 0; i < m; i++) {
+                while (!stack.empty() && left[stack.peek()][j] >= numbers[i][j]) {
+                    stack.pop();
+                }
+                up[i] = stack.empty() ? -1 : stack.peek();
+                stack.push(i);
+            }
+            stack.clear();
+            for (int i = m - 1; i >= 0; i--) {
+                while (!stack.empty() && left[stack.peek()][j] >= numbers[i][j]) {
+                    stack.pop();
+                }
+                down[i] = stack.empty() ? -1 : stack.peek();
+                stack.push(i);
+            }
+
+            for (int i = 0; i < m; i++) {
+                int high = down[i] - up[i] - 1;
+                int are = high * left[i][j];
+                ret = Math.max(ret, are);
+            }
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int[][] numbers = {{1, 0, 1, 0, 0}, {1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0}};
+        int res = maxArea(numbers);
+        System.out.println(res);
+    }
+}
