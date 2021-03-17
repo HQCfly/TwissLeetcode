3342e5b313062adc675c3307a9ac80916d3f96e5
HQCfly <2249629867@qq.com>
2021-03-17 22:00:12 +0800

    Add MergeInterval

--- src/main/java/com/twiss/arraypractice/MergeIntervals.java
+++ src/main/java/com/twiss/arraypractice/MergeIntervals.java
@@ -2,10 +2,7 @@ package com.twiss.arraypractice;
 
 import com.alibaba.fastjson.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: Twiss
@@ -26,17 +23,82 @@ public class MergeIntervals {
             int right = intervals[i][1];
             while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
                 i++;
-                right = Math.max(right,intervals[i][1]);
+                right = Math.max(right, intervals[i][1]);
             }
-            res.add(new int[]{left,right});
+            res.add(new int[]{left, right});
             i++;
         }
         return res.toArray(new int[0][]);
+    }
+
+    public static List<Interval> merge2(List<Interval> intervals){
+        List<Interval> res = new ArrayList<Interval>();
+        Collections.sort(intervals, new Comparator<Interval>() { public int compare(Interval a, Interval b) {
+            return a.start - b.start; }
+        });
+
+        Stack<Interval> stack = new Stack<Interval>();
+        for (Interval interval:intervals){
+            if (stack.empty()||interval.start>stack.peek().end){
+                stack.push(interval);
+            }else {
+                stack.peek().end = Math.max(interval.end,stack.peek().end);
+            }
+        }
+
+        while (!stack.empty()){
+            res.add(stack.pop());
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
         int[][] res = merge(intervals);
         System.out.println(JSONObject.toJSONString(res));
+
+        Interval interval = new Interval(1,3);
+        Interval interval2 = new Interval(2,6);
+        Interval interval3 = new Interval(8,10);
+        Interval interval4 = new Interval(15,18);
+        List<Interval> timeIntervals = new ArrayList<Interval>();
+        timeIntervals.add(interval);
+        timeIntervals.add(interval2);
+        timeIntervals.add(interval3);
+        timeIntervals.add(interval4);
+        List<Interval> res2 = merge2(timeIntervals);
+        System.out.println(JSONObject.toJSONString(res2));
+    }
+}
+
+class Interval {
+    int start;
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
     }
 }
---
+++ src/main/java/com/twiss/arraypractice/MergeSortedArray.java
@@ -0,0 +1,9 @@
+package com.twiss.arraypractice;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/3/17 9:22 下午
+ */
+public class MergeSortedArray {
+
+}
