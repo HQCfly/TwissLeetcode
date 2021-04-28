1e03d13748103680402bff34fbe261939ab677e5
HQCfly <2249629867@qq.com>
2021-04-28 23:26:18 +0800

    Add uniquePath

--- src/main/java/com/twiss/arraypractice/UniquePaths.java
+++ src/main/java/com/twiss/arraypractice/UniquePaths.java
@@ -5,4 +5,40 @@ package com.twiss.arraypractice;
  * @Date: 2021/4/27 8:43 下午
  */
 public class UniquePaths {
+
+    public static int getSumOfPaths(int m, int n) {
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = f[i-1][j]+f[i][j-1];
+            }
+        }
+
+        return f[m - 1][n - 1];
+    }
+
+    public static int getSumOfPathsInMath(int m, int n) {
+        int ans = 1;
+        for (int x=n,y=1;y<m;++x,++y){
+            ans = ans * x/y;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 7;
+        int res = getSumOfPaths(m, n);
+        int res2 = getSumOfPathsInMath(m, n);
+        System.out.println(res);
+        System.out.println(res2);
+    }
 }
