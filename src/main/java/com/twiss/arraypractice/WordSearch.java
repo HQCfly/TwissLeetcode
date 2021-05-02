f3e9c333e736067e2df1c5f49f77b8604e52d426
HQCfly <2249629867@qq.com>
2021-05-02 19:55:48 +0800

    Add word serach

---
+++ src/main/java/com/twiss/arraypractice/WordSearch.java
@@ -0,0 +1,15 @@
+package com.twiss.arraypractice;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/5/2 7:54 下午
+ */
+public class WordSearch {
+    public static void main(String[] args) {
+        String[][] word = {
+            {"A","B","C","E"},
+            {"S","F","C","S"},
+            {"A","D","E","E"}
+        };
+    }
+}
