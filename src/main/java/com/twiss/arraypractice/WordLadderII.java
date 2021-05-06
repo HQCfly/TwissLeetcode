220d1b4e8c662e6debd99768615cd29ab876a58c
HQCfly <2249629867@qq.com>
2021-05-06 22:36:35 +0800

    Add wordLadderII

--- src/main/java/com/twiss/arraypractice/WordLadderII.java
+++ src/main/java/com/twiss/arraypractice/WordLadderII.java
@@ -1,5 +1,7 @@
 package com.twiss.arraypractice;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.*;
 
 /**
@@ -8,167 +10,96 @@ import java.util.*;
  */
 public class WordLadderII {
 
-    public static int getLadderLength(String beginWord,
-                                      String endWord,
-                                      List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList);
-        // 判断是否wordList是空，或者不包括endBegin
-        if (wordList.size() == 0 || !wordSet.contains(endWord)) {
-            return 0;
+    public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> res = new ArrayList<>();
+        // 因为需要快速判断扩展出的单词是否在 wordList 里，因此需要将 wordList 存入哈希表，这里命名为「字典」
+        Set<String> dict = new HashSet<>(wordList);
+        // 特殊用例判断
+        if (!dict.contains(endWord)) {
+            return res;
         }
-        wordSet.remove(beginWord);
-        // 第 2 步：图的广度优先遍历，必须使用队列和表示是否访问过的 visited 哈希表
+
+        dict.remove(beginWord);
+
+        // 第 1 步：广度优先遍历建图
+        // 记录扩展出的单词是在第几次扩展的时候得到的，key：单词，value：在广度优先遍历的第几层
+        Map<String, Integer> steps = new HashMap<>();
+        steps.put(beginWord, 0);
+        // 记录了单词是从哪些单词扩展而来，key：单词，value：单词列表，这些单词可以变换到 key ，它们是一对多关系
+        Map<String, List<String>> from = new HashMap<>();
+        int step = 1;
+        boolean found = false;
+        int wordLen = beginWord.length();
         Queue<String> queue = new LinkedList<>();
-        // - 将beginWord作为队首
         queue.offer(beginWord);
-        Set<String> visited = new HashSet<>();
-        // - 将beginWord添加为访问过
-        visited.add(beginWord);
-
-        // 第 3 步：开始广度优先遍历，包含起点，因此初始化的时候步数为 1
-        int step = 1;
         while (!queue.isEmpty()) {
-            int currentSize = queue.size();
-            for (int i = 0; i < currentSize; ++i) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
                 // 依次遍历队列中的单词
-                String currentWord = queue.poll();
-                // 如果currentWord能够修改一个字符就能与endWord相等则返回step+1
-                if (changeWordEveryOneLetter(currentWord,
-                        endWord, queue, visited, wordSet)) {
-                    return step + 1;
+                String currWord = queue.poll();
+                char[] charArray = currWord.toCharArray();
+                // 将每一位替换成 26 个小写英文字母
+                for (int j = 0; j < wordLen; j++) {
+                    char origin = charArray[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        charArray[j] = c;
+                        String nextWord = String.valueOf(charArray);
+                        if (steps.containsKey(nextWord) && step == steps.get(nextWord)) {
+                            from.get(nextWord).add(currWord);
+                        }
+                        if (!dict.contains(nextWord)) {
+                            continue;
+                        }
+                        // 如果从一个单词扩展出来的单词以前遍历过，距离一定更远，为了避免搜索到已经遍历到，且距离更远的单词，需要将它从 dict 中删除
+                        dict.remove(nextWord);
+                        // 这一层扩展出的单词进入队列
+                        queue.offer(nextWord);
+
+                        // 记录 nextWord 从 currWord 而来
+                        from.putIfAbsent(nextWord, new ArrayList<>());
+                        from.get(nextWord).add(currWord);
+                        // 记录 nextWord 的 step
+                        steps.put(nextWord, step);
+                        if (nextWord.equals(endWord)) {
+                            found = true;
+                        }
+                    }
+                    charArray[j] = origin;
                 }
             }
             step++;
-        }
-        return 0;
-    }
-    public static boolean changeWordEveryOneLetter(String currentWord,
-                                                   String endWord,
-                                                   Queue<String> queue,
-                                                   Set<String> visited,
-                                                   Set<String> wordSet) {
-        char[] charArray = currentWord.toCharArray();
-        for (int i = 0; i < endWord.length(); ++i) {
-            // 先保存，然后恢复
-            char originChar = charArray[i];
-            // 从a遍历z
-            for (char k = 'a'; k <= 'z'; k++) {
-                if (k == originChar) {
-                    continue;
-                }
-                charArray[i] = k;
-                String nextWord = String.valueOf(charArray);
-                // 判断集合中是否存在组合后的新单词
-                if (wordSet.contains(nextWord)) {
-                    if (nextWord.equals(endWord)) {
-                        return true;
-                    }
-                    // 如果nextWord没有被访问过
-                    if (!visited.contains(nextWord)) {
-                        // 注意：添加到队列以后，必须马上标记为已经访问
-                        queue.add(nextWord);
-                        // 添加到访问集合
-                        visited.add(nextWord);
-                    }
-                }
+            if (found) {
+                break;
             }
-            // 恢复初始
-            charArray[i] = originChar;
         }
-        return false;
+
+        // 第 2 步：深度优先遍历找到所有解，从 endWord 恢复到 beginWord ，所以每次尝试操作 path 列表的头部
+        if (found) {
+            Deque<String> path = new ArrayDeque<>();
+            path.add(endWord);
+            dfs(from, path, beginWord, endWord, res);
+        }
+        return res;
     }
 
-    public static int getLadderLength2(String beginWord,
-                                       String endWord,
-                                       List<String> wordList) {
-        // 第 1 步：先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
-        Set<String> wordSet = new HashSet<>(wordList);
-        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
-            return 0;
+    public static void dfs(Map<String, List<String>> from, Deque<String> path, String beginWord, String cur, List<List<String>> res) {
+        if (cur.equals(beginWord)) {
+            res.add(new ArrayList<>(path));
+            return;
         }
-
-        // 第 2 步：已经访问过的 word 添加到 visited 哈希表里
-        Set<String> visited = new HashSet<>();
-        // 分别用左边和右边扩散的哈希表代替单向 BFS 里的队列，它们在双向 BFS 的过程中交替使用
-        Set<String> beginVisited = new HashSet<>();
-        beginVisited.add(beginWord);
-        Set<String> endVisited = new HashSet<>();
-        endVisited.add(endWord);
-
-        // 第 3 步：执行双向 BFS，左右交替扩散的步数之和为所求
-        int step = 1;
-        while (!beginVisited.isEmpty() && !endVisited.isEmpty()) {
-            // 优先选择小的哈希表进行扩散，考虑到的情况更少
-            if (beginVisited.size() > endVisited.size()) {
-                Set<String> temp = beginVisited;
-                beginVisited = endVisited;
-                endVisited = temp;
-            }
-
-            // 逻辑到这里，保证 beginVisited 是相对较小的集合，nextLevelVisited 在扩散完成以后，会成为新的 beginVisited
-            Set<String> nextLevelVisited = new HashSet<>();
-            for (String word : beginVisited) {
-                if (changeWordEveryOneLetter2(word, endVisited, visited, wordSet, nextLevelVisited)) {
-                    return step + 1;
-                }
-            }
-
-            // 原来的 beginVisited 废弃，从 nextLevelVisited 开始新的双向 BFS
-            beginVisited = nextLevelVisited;
-            step++;
+        for (String precursor : from.get(cur)) {
+            path.addFirst(precursor);
+            dfs(from, path, beginWord, precursor, res);
+            path.removeFirst();
         }
-        return 0;
-    }
-    /**
-     * 尝试对 word 修改每一个字符，看看是不是能落在 endVisited 中，扩展得到的新的 word 添加到 nextLevelVisited 里
-     *
-     * @param word
-     * @param endVisited
-     * @param visited
-     * @param wordSet
-     * @param nextLevelVisited
-     * @return
-     */
-    private static boolean changeWordEveryOneLetter2(String word, Set<String> endVisited,
-                                                     Set<String> visited,
-                                                     Set<String> wordSet,
-                                                     Set<String> nextLevelVisited) {
-        char[] charArray = word.toCharArray();
-        for (int i = 0; i < word.length(); i++) {
-            char originChar = charArray[i];
-            for (char c = 'a'; c <= 'z'; c++) {
-                if (originChar == c) {
-                    continue;
-                }
-                charArray[i] = c;
-                String nextWord = String.valueOf(charArray);
-                if (wordSet.contains(nextWord)) {
-                    if (endVisited.contains(nextWord)) {
-                        return true;
-                    }
-                    if (!visited.contains(nextWord)) {
-                        nextLevelVisited.add(nextWord);
-                        visited.add(nextWord);
-                    }
-                }
-            }
-            // 恢复，下次再用
-            charArray[i] = originChar;
-        }
-        return false;
     }
 
     public static void main(String[] args) {
         List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log","cog");
         String beginWord = "hit";
         String endWord = "cog";
-        int res = getLadderLength(beginWord, endWord, wordList);
-        System.out.println(res);
+        List<List<String>> res = findLadders(beginWord, endWord, wordList);
+        System.out.println(JSONObject.toJSONString(res));
 
-        List<String> wordList2 = Arrays.asList("hot", "dot", "dog", "lot", "log","cog");
-        String beginWord2 = "hit";
-        String endWord2 = "cog";
-        int res2 = getLadderLength2(beginWord2, endWord2, wordList2);
-        System.out.println(res2);
     }
 }
