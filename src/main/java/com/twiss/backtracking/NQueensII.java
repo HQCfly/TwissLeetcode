cb677b4cbdca0ed549048e44e07b40454264e020
HQCfly <2249629867@qq.com>
2021-05-25 19:56:23 +0800

    Add NQueensII

---
+++ src/main/java/com/twiss/backtracking/NQueensII.java
@@ -0,0 +1,52 @@
+package com.twiss.backtracking;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/5/23 9:08 下午
+ */
+public class NQueensII {
+    public int count = 0;
+
+    private int solveQueens(int n) {
+        List<String[]> res = new ArrayList<>();
+        int[] columnRow = new int[n];
+        backtrace(n, 0, columnRow);
+        return count;
+    }
+
+    private void backtrace(int n, int row, int[] columnRow) {
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            columnRow[row] = i;
+            if (!isValid(i, row, columnRow)) {
+                continue;
+            }
+            backtrace(n, row + 1, columnRow);
+        }
+    }
+
+    private boolean isValid(int col, int row, int[] columnRow) {
+        for (int r = 0; r < row; r++) {
+            if (columnRow[r] == col || row - r == Math.abs(columnRow[r] - col)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int res2 = new NQueensII().solveQueens(n);
+        System.out.println(res2);
+    }
+}
