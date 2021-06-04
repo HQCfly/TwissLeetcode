cc17a88fc50e32abd8b5bba61211b09145d6d925
HQCfly <2249629867@qq.com>
2021-06-04 15:41:08 +0800

    Add restore ip address

---
+++ src/main/java/com/twiss/backtracking/RestoreIPAddress.java
@@ -0,0 +1,14 @@
+package com.twiss.backtracking;
+
+import java.util.List;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/6/3 4:09 下午
+ */
+public class RestoreIPAddress {
+    public static void main(String[] args) {
+        String s = "25525511135";
+        List<String> res = new RestoreIPAddress().getNewIPAddress();
+    }
+}
