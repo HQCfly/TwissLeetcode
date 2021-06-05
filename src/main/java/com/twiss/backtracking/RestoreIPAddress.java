78e583142d6e6389722932f09567ccef2488fe54
HQCfly <2249629867@qq.com>
2021-06-05 21:56:39 +0800

    Add getNewIPAddress

--- src/main/java/com/twiss/backtracking/RestoreIPAddress.java
+++ src/main/java/com/twiss/backtracking/RestoreIPAddress.java
@@ -1,5 +1,6 @@
 package com.twiss.backtracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,6 +8,13 @@ import java.util.List;
  * @Date: 2021/6/3 4:09 下午
  */
 public class RestoreIPAddress {
+    
+    private List<String> getNewIPAddress() {
+        List<String> res = new ArrayList<>();
+
+        return res;
+    }
+
     public static void main(String[] args) {
         String s = "25525511135";
         List<String> res = new RestoreIPAddress().getNewIPAddress();
