62705fc699792147a07b2eca9144840578e4ef67
HQCfly <2249629867@qq.com>
2021-06-07 23:23:30 +0800

    Add function in RestoreIPAddress

--- src/main/java/com/twiss/backtracking/RestoreIPAddress.java
+++ src/main/java/com/twiss/backtracking/RestoreIPAddress.java
@@ -1,6 +1,7 @@
 package com.twiss.backtracking;
 
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -8,21 +9,30 @@ import java.util.List;
  * @Date: 2021/6/3 4:09 下午
  */
 public class RestoreIPAddress {
-    
+
     private List<String> getNewIPAddress(String ip) {
         List<String> res = new ArrayList<>();
 
         return res;
     }
 
-    private void dfs(int n, String cur, List<String> res){
-        if (cur.length()==n){
-            res.add(cur);
+    private void dfs(int n, int start, int residue, String ip, Deque<String> path, List<String> res) {
+        if (start == n) {
+            if (residue==0){
+                res.add(String.join(".",path));
+            }
             return;
         }
 
+        for (int i = start; i < start+3; i++) {
 
+        }
 
+    }
+
+    private boolean judgeIpSegment(String tmpIP){
+
+        return true;
     }
 
     public static void main(String[] args) {
