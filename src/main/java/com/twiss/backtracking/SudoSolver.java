fda127359bc7bff8b9a766e465471c15d9f1151d
HQCfly <2249629867@qq.com>
2021-06-12 22:19:27 +0800

    Add solver in sudo

--- src/main/java/com/twiss/backtracking/SudoSolver.java
+++ src/main/java/com/twiss/backtracking/SudoSolver.java
@@ -6,6 +6,21 @@ package com.twiss.backtracking;
  */
 public class SudoSolver {
 
+    public void solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    spaces.add(new int[]{i, j});
+                } else {
+                    int digit = board[i][j] - '0' - 1;
+                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                }
+            }
+        }
+
+        dfs(board, 0);
+    }
+
     public static void main(String[] args) {
 
     }
