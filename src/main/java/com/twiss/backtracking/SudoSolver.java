b0153094131836daeb520bdc9f5497f8601ca525
HQCfly <2249629867@qq.com>
2021-06-14 23:04:59 +0800

    Add function in sudo

--- src/main/java/com/twiss/backtracking/SudoSolver.java
+++ src/main/java/com/twiss/backtracking/SudoSolver.java
@@ -1,5 +1,7 @@
 package com.twiss.backtracking;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +31,7 @@ public class SudoSolver {
 
         dfs(board, 0);
     }
+
     public void dfs(char[][] board, int pos) {
         if (pos == spaces.size()) {
             valid = true;
@@ -46,10 +49,20 @@ public class SudoSolver {
             }
         }
     }
+
     public static void main(String[] args) {
-        int n = 9;
-        char[][] board = new char[n][n];
+        char[][] board ={{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
-
+        SudoSolver sudoSolver = new SudoSolver();
+        sudoSolver.solveSudoku(board);
+        System.out.println(JSONObject.toJSONString(sudoSolver.spaces));
     }
 }
