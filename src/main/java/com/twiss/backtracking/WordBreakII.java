6cbc0b11c96c4e7ffb50530f86a8c5bd586f0597
HQCfly <2249629867@qq.com>
2021-06-26 18:39:48 +0800

    Modify wordBreak

--- src/main/java/com/twiss/backtracking/WordBreakII.java
+++ src/main/java/com/twiss/backtracking/WordBreakII.java
@@ -1,14 +1,28 @@
 package com.twiss.backtracking;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Twiss
  * @Date: 2021/6/23 9:57 下午
  */
 public class WordBreakII {
 
+    private List<String> getResult(String words,List<String> wordDict) {
+
+
+        return new ArrayList<>();
+    }
+
 
     public static void main(String[] args) {
-        String s = "";
+        String words = "";
+        List<String> wordDict = new ArrayList<String>();
+        List<String> res = new WordBreakII().getResult(words,wordDict);
+        System.out.println(JSONObject.toJSONString(res));
     }
 
 }
