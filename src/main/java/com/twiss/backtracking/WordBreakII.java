93b34dff243f67e7f73edce000dd17ed7656c96a
HQCfly <2249629867@qq.com>
2021-06-27 22:25:11 +0800

    Add comment in  WordBreakII

--- src/main/java/com/twiss/backtracking/WordBreakII.java
+++ src/main/java/com/twiss/backtracking/WordBreakII.java
@@ -14,6 +14,7 @@ public class WordBreakII {
     /**
      * 先使用动态规划将每个可截取单词的right下标找出来dp[right] = true
      * 其次对整个字符串进行dfs回溯算法查找所有有效的分词。根据是否存在词典中和dp[i]是否是true，如果true则往下搜索
+     * -- 符合条件的分词使用Deque双端队列
      */
     private List<String> getResult(String words,List<String> wordDict) {
 
