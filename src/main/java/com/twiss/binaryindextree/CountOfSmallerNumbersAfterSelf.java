576158f1af4dac242cd3051165b02d55e31766dc
HQCfly <2249629867@qq.com>
2021-07-02 22:34:34 +0800

    Add countSmaller

--- src/main/java/com/twiss/binaryindextree/CountOfSmallerNumbersAfterSelf.java
+++ src/main/java/com/twiss/binaryindextree/CountOfSmallerNumbersAfterSelf.java
@@ -1,10 +1,19 @@
 package com.twiss.binaryindextree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Twiss
  * @Date: 2021/6/29 11:36 下午
  */
 public class CountOfSmallerNumbersAfterSelf {
+
+    private List<Integer> countSmaller(int[] nums) {
+        List<Integer> resultList = new ArrayList<>();
+
+        return resultList;
+    }
 
     public static void main(String[] args) {
         int[] number = {5,2,6,1};
