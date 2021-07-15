2203dc59ab61215d73ba329f4119c20876d182a0
HQCfly <2249629867@qq.com>
2021-07-15 16:56:30 +0800

    Add public class ClosestBinarySearchTreeValue

---
+++ src/main/java/com/twiss/binarysearch/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,30 @@
+package com.twiss.binarysearch;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/7/15 4:14 下午
+ */
+public class ClosestBinarySearchTreeValue {
+
+    private int getClosestNumber(TreeNode treeNode,int target){
+        int result = treeNode.val;
+        while (treeNode!=null){
+            if (Math.abs(treeNode.val-target)<Math.abs(result-target)){
+                result = treeNode.val;
+            }
+            treeNode = treeNode.val>target?treeNode.left:treeNode.right;
+        }
+        return result;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
