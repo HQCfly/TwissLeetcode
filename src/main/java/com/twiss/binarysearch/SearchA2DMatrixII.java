1b8ec89f762d732a3307992dbdf6476ed7165a69
HQCfly <2249629867@qq.com>
2021-08-01 23:19:00 +0800

    Add searchMatrixByBinarySearchRowAndColumn

--- src/main/java/com/twiss/binarysearch/SearchA2DMatrixII.java
+++ src/main/java/com/twiss/binarysearch/SearchA2DMatrixII.java
@@ -7,9 +7,57 @@ package com.twiss.binarysearch;
 public class SearchA2DMatrixII {
 
     public boolean searchMatrixByForce(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[1].length; j++) {
                 if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean searchMatrixByBinarySearchRowAndColumn(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int shorterDim = Math.min(matrix.length, matrix[0].length);
+        for (int i = 0; i < shorterDim; i++) {
+            boolean rowVertical = binarySearchRowAndColumn(matrix,target,i,true);
+            boolean columnVertical = binarySearchRowAndColumn(matrix,target,i,false);
+            if (rowVertical||columnVertical){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean binarySearchRowAndColumn(int[][] matrix, int target,
+                                             int start, boolean vertical) {
+        int low = start;
+        int high = vertical ? matrix.length-1:matrix[0].length-1;
+
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (vertical){
+                // search row
+                if (matrix[mid][start]>target){
+                    high = mid-1;
+                } else if (matrix[mid][start]<target){
+                    low = mid+1;
+                } else {
+                    return true;
+                }
+            } else {
+                // search column
+                if (matrix[start][mid]>target){
+                    high = mid-1;
+                } else if (matrix[start][mid]<target){
+                    low = mid+1;
+                } else {
                     return true;
                 }
             }
@@ -26,7 +74,10 @@ public class SearchA2DMatrixII {
                 {18, 21, 23, 26, 30}
         };
         int target = 5;
-        boolean res = new SearchA2DMatrixII().searchMatrixByForce(matrix,target);
+        boolean res = new SearchA2DMatrixII().searchMatrixByForce(matrix, target);
         System.out.println(res);
+
+        boolean res2 = new SearchA2DMatrixII().searchMatrixByBinarySearchRowAndColumn(matrix,target);
+        System.out.println(res2);
     }
 }
