5432f73de92eb2cdb87b0693e811c4e7a470a1a5
HQCfly <2249629867@qq.com>
2021-08-19 20:27:07 +0800

    Add BinaryTreeLevelOrderTraversal

---
+++ src/main/java/com/twiss/bfs/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,64 @@
+package com.twiss.bfs;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/8/19 6:18 下午
+ */
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> list = new ArrayList<>();
+        if (root==null){
+            return list;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            List<Integer> leve = new ArrayList<>();
+            int currentLeveSie = queue.size();
+            for (int i=1;i<=currentLeveSie;++i){
+                // 将当前的节点取出加入到leve中
+                TreeNode currentNode = queue.poll();
+                leve.add(currentNode.val);
+                // 如果当前节点存在左节点，将其加入queue中进行下层次循环
+                if (currentNode.left!=null){
+                    queue.offer(currentNode.left);
+                }
+                // 如果当前节点存在右节点，将其加入queue中进行下层次循环
+                if (currentNode.right!=null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            list.add(leve);
+        }
+        return list;
+    }
+
+    /**
+     *          3
+     *         / \
+     *        9   20
+     *       /     \
+     *      15      7
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 构建树
+        TreeNode treeNode = new TreeNode(3);
+        TreeNode rightNode = new TreeNode(20,new TreeNode(15),new TreeNode(7));
+        treeNode.left = new TreeNode(9);
+        treeNode.right = rightNode;
+
+        List<List<Integer>> res = new BinaryTreeLevelOrderTraversal().levelOrder(treeNode);
+        System.out.println(JSONObject.toJSONString(res));
+
+    }
+}
---
+++ src/main/java/com/twiss/bfs/TreeNode.java
@@ -0,0 +1,24 @@
+package com.twiss.bfs;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/7/15 5:21 下午
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
