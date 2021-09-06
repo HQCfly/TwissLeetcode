e41c582b4e226305e4eae6af7e1a59701ebf3258
HQCfly <2249629867@qq.com>
2021-09-06 22:33:57 +0800

    Add min stack and RemoveDuplicateLetters

---
+++ src/main/java/com/twiss/stack/MinStack.java
@@ -0,0 +1,49 @@
+package com.twiss.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/9/5 2:03 下午
+ */
+public class MinStack {
+
+    private Deque<Integer> stackElement = new ArrayDeque<>();
+    private Deque<Integer> stackMinElement = new ArrayDeque<>();
+
+    public MinStack() {
+        stackMinElement.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stackElement.push(val);
+        stackMinElement.push(Math.min(val,stackElement.peek()));
+    }
+
+    public void pop() {
+        stackElement.pop();
+        stackMinElement.pop();
+    }
+
+    public int top() {
+        return stackElement.peek();
+    }
+
+    public int getMin() {
+        return stackMinElement.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println("originStack: "+minStack.stackElement);
+        int minElement = minStack.getMin();
+        System.out.println("minElement: "+minElement);
+        minStack.pop();
+        minStack.top();
+        minStack.getMin();
+    }
+}
---
+++ src/main/java/com/twiss/stack/RemoveDuplicateLetters.java
@@ -0,0 +1,56 @@
+package com.twiss.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/9/6 10:09 下午
+ */
+public class RemoveDuplicateLetters {
+
+    public String removeDuplicateLetters(String letters){
+        // 存放去重结果
+        Deque<Character> stack = new ArrayDeque<>();
+        // 记录出现字符次数
+        int[] count = new int[256];
+
+        for (int i=0;i<letters.length();++i){
+            count[letters.charAt(i)]++;
+        }
+        // 记录栈中是否存在某个字符
+        boolean[] inStack = new boolean[256];
+        for (char c:letters.toCharArray()){
+            // 每遍历一个字符，都将对应对应计数减1
+            count[c]--;
+
+            // 如果栈中存在c字符，则跳过
+            if (inStack[c]){
+                continue;
+            }
+
+            while ((!stack.isEmpty())&&(stack.peek()>c)){
+                if (count[stack.peek()]==0){
+                    break;
+                }
+                // 若之后还有，则可以 pop
+                inStack[stack.pop()]=false;
+            }
+
+            stack.push(c);
+            inStack[c]=true;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!stack.isEmpty()){
+            stringBuilder.append(stack.pop());
+        }
+        return stringBuilder.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String letters = "bcac";
+        String res = new RemoveDuplicateLetters().removeDuplicateLetters(letters);
+        System.out.println(res);
+    }
+}
