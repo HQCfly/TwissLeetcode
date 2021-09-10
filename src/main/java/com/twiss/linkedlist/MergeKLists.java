963864fc9866b5e500c7b1415cb96d376fcecf2d
HQCfly <2249629867@qq.com>
2021-09-10 23:58:46 +0800

    Add merge k list

---
+++ src/main/java/com/twiss/linkedlist/MergeKLists.java
@@ -0,0 +1,41 @@
+package com.twiss.linkedlist;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/9/10 11:21 下午
+ */
+public class MergeKLists {
+
+    public LinkedNode mergeLinkedNode(LinkedNode[] lists){
+        LinkedNode ans = null;
+        for (int i=0;i<lists.length;++i){
+            ans = merge(ans,lists[i]);
+        }
+        return ans;
+    }
+
+    private LinkedNode merge(LinkedNode linkedNodeA,LinkedNode linkedNodeB){
+        if (linkedNodeA==null||linkedNodeB==null){
+            return linkedNodeA==null?linkedNodeB:linkedNodeA;
+        }
+
+        LinkedNode head = new LinkedNode(0);
+        LinkedNode tail = head, aPtr = linkedNodeA, bPtr = linkedNodeB;
+        while (aPtr!=null||bPtr!=null){
+            if (aPtr.val>bPtr.val){
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            }else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr!=null?aPtr:bPtr);
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
