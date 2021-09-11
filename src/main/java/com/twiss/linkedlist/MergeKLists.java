c2b72d8e8da549bbb3642216a12fb562d82bdcff
HQCfly <2249629867@qq.com>
2021-09-11 22:00:07 +0800

    codefoemat

--- src/main/java/com/twiss/linkedlist/MergeKLists.java
+++ src/main/java/com/twiss/linkedlist/MergeKLists.java
@@ -6,35 +6,36 @@ package com.twiss.linkedlist;
  */
 public class MergeKLists {
 
-    public LinkedNode mergeLinkedNode(LinkedNode[] lists){
+    public LinkedNode mergeLinkedNode(LinkedNode[] lists) {
         LinkedNode ans = null;
-        for (int i=0;i<lists.length;++i){
-            ans = merge(ans,lists[i]);
+        for (int i = 0; i < lists.length; ++i) {
+            ans = merge(ans, lists[i]);
         }
         return ans;
     }
 
-    private LinkedNode merge(LinkedNode linkedNodeA,LinkedNode linkedNodeB){
-        if (linkedNodeA==null||linkedNodeB==null){
-            return linkedNodeA==null?linkedNodeB:linkedNodeA;
+    private LinkedNode merge(LinkedNode linkedNodeA, LinkedNode linkedNodeB) {
+        if (linkedNodeA == null || linkedNodeB == null) {
+            return linkedNodeA == null ? linkedNodeB : linkedNodeA;
         }
 
         LinkedNode head = new LinkedNode(0);
         LinkedNode tail = head, aPtr = linkedNodeA, bPtr = linkedNodeB;
-        while (aPtr!=null||bPtr!=null){
-            if (aPtr.val>bPtr.val){
+        while (aPtr != null || bPtr != null) {
+            if (aPtr.val > bPtr.val) {
                 tail.next = aPtr;
                 aPtr = aPtr.next;
-            }else {
+            } else {
                 tail.next = bPtr;
                 bPtr = bPtr.next;
             }
             tail = tail.next;
         }
-        tail.next = (aPtr!=null?aPtr:bPtr);
+        tail.next = (aPtr != null ? aPtr : bPtr);
         return head.next;
     }
 
-    public static void main(String[] args
+    public static void main(String[] args) {
+
     }
 }
