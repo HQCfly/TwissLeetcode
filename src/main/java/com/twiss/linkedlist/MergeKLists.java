0af56b6dc9470240c6fdd43f00c5c72829463c14
HQCfly <2249629867@qq.com>
2021-09-12 23:13:39 +0800

    Add test in merge k list

--- src/main/java/com/twiss/linkedlist/MergeKLists.java
+++ src/main/java/com/twiss/linkedlist/MergeKLists.java
@@ -1,5 +1,7 @@
 package com.twiss.linkedlist;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * @Author: Twiss
  * @Date: 2021/9/10 11:21 下午
@@ -9,20 +11,19 @@ public class MergeKLists {
     public LinkedNode mergeLinkedNode(LinkedNode[] lists) {
         LinkedNode ans = null;
         for (int i = 0; i < lists.length; ++i) {
-            ans = merge(ans, lists[i]);
+            ans = mergeTwoLists(ans, lists[i]);
         }
         return ans;
     }
 
-    private LinkedNode merge(LinkedNode linkedNodeA, LinkedNode linkedNodeB) {
-        if (linkedNodeA == null || linkedNodeB == null) {
-            return linkedNodeA == null ? linkedNodeB : linkedNodeA;
+    private LinkedNode mergeTwoLists(LinkedNode a, LinkedNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
         }
-
         LinkedNode head = new LinkedNode(0);
-        LinkedNode tail = head, aPtr = linkedNodeA, bPtr = linkedNodeB;
-        while (aPtr != null || bPtr != null) {
-            if (aPtr.val > bPtr.val) {
+        LinkedNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
                 tail.next = aPtr;
                 aPtr = aPtr.next;
             } else {
@@ -36,6 +37,17 @@ public class MergeKLists {
     }
 
     public static void main(String[] args) {
+        // 1,4,5
+        LinkedNode node1 = new LinkedNode(1,new LinkedNode(4,new LinkedNode(5)));
+        // 1,3,4
+        LinkedNode node2 = new LinkedNode(1,new LinkedNode(3,new LinkedNode(4)));
+        // 2,6
+        LinkedNode node3 = new LinkedNode(2,new LinkedNode(6));
+
+        LinkedNode[] list = {node1,node2,node3};
+
+        LinkedNode res = new MergeKLists().mergeLinkedNode(list);
+        System.out.println(JSONObject.toJSONString(res));
 
     }
 }
