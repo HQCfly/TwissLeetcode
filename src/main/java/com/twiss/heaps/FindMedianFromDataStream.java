2257a8834607efca4d6688744343c278dfc564e7
HQCfly <2249629867@qq.com>
2021-09-18 18:44:12 +0800

    Modified FindMedianFromDataStream

--- src/main/java/com/twiss/heaps/FindMedianFromDataStream.java
+++ src/main/java/com/twiss/heaps/FindMedianFromDataStream.java
@@ -1,10 +1,15 @@
 package com.twiss.heaps;
 
+import java.util.PriorityQueue;
+
 /**
  * @Author: Twiss
  * @Date: 2021/9/16 7:09 下午
  */
 public class FindMedianFromDataStream {
+    PriorityQueue<Integer> l = new PriorityQueue<>((a, b)->b-a);
+    PriorityQueue<Integer> r = new PriorityQueue<>((a,b)->a-b);
+
     public static void main(String[] args) {
 
     }
