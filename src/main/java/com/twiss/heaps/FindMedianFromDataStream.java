58dad7f6461d375670b2000e91c1a003ade02eb9
HQCfly <2249629867@qq.com>
2021-09-19 20:25:31 +0800

    Add FindMedianFromDataStream

--- src/main/java/com/twiss/heaps/FindMedianFromDataStream.java
+++ src/main/java/com/twiss/heaps/FindMedianFromDataStream.java
@@ -10,7 +10,45 @@ public class FindMedianFromDataStream {
     PriorityQueue<Integer> l = new PriorityQueue<>((a, b)->b-a);
     PriorityQueue<Integer> r = new PriorityQueue<>((a,b)->a-b);
 
-    public static void main(String[] args) {
+    public void addNum(int num){
+        int s1 = l.size(), s2 = r.size();
+        // 当数据流元素数量为偶数：l 和 r 大小相同，此时动态中位数为两者堆顶元素的平均值；
+        if (s1==s2){
+            if (r.isEmpty()||num<=r.peek()){
+                l.add(num);
+            }else {
+                l.add(r.poll());
+                r.add(num);
+            }
+        }else {
+            // 当数据流元素数量为奇数：l 比 r 多一，此时动态中位数为 l 的堆顶原数。
+            if (l.peek()<=num){
+                r.add(num);
+            }else {
+                r.add(l.poll());
+                l.add(num);
+            }
+        }
+    }
 
+    public double findMedian(){
+        int s1 = l.size(),s2=r.size();
+        if (s1==s2){
+            return (l.peek()+r.peek())/2.0;
+        }else {
+            return l.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        FindMedianFromDataStream ds = new FindMedianFromDataStream();
+
+        ds.addNum(1);
+        ds.addNum(2);
+        double mediaNum = ds.findMedian();
+        System.out.println("mediaNum:"+mediaNum);
+        ds.addNum(3);
+        double mediaNum2 = ds.findMedian();
+        System.out.println("mediaNum2:"+mediaNum2);
     }
 }
