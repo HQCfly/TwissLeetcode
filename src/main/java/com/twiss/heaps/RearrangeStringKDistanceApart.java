7fdac001577bbe64157fcdcc6fafc8c0f59d371e
HQCfly <2249629867@qq.com>
2021-09-28 22:52:14 +0800

    Modified RearrangeStringKDistanceApart

--- src/main/java/com/twiss/heaps/RearrangeStringKDistanceApart.java
+++ src/main/java/com/twiss/heaps/RearrangeStringKDistanceApart.java
@@ -1,10 +1,18 @@
 package com.twiss.heaps;
 
+import java.util.PriorityQueue;
+
 /**
  * @Author: Twiss
  * @Date: 2021/9/27 11:04 下午
  */
 public class RearrangeStringKDistanceApart {
+
+    public String rearrangeString(String str, int k){
+
+        PriorityQueue<Character> priorityQueue = new PriorityQueue<>((a, b)->b-a);
+
+    }
 
     public static void main(String[] args) {
 
