4fa84d11e0e326e7f98fd28a22cf93d74fea0648
HQCfly <2249629867@qq.com>
2021-10-14 22:36:28 +0800

    Add show in max and min heap

--- src/main/java/com/twiss/xiaohuang/cp4heap/UnsortedArrayConvertMaxHeap.java
+++ src/main/java/com/twiss/xiaohuang/cp4heap/UnsortedArrayConvertMaxHeap.java
@@ -1,6 +1,7 @@
 package com.twiss.xiaohuang.cp4heap;
 
 import com.alibaba.fastjson.JSONObject;
+import com.twiss.xiaohuang.util.tree.OperationBinaryTree;
 
 /**
  * @Author: Twiss
@@ -8,17 +9,17 @@ import com.alibaba.fastjson.JSONObject;
  */
 public class UnsortedArrayConvertMaxHeap {
 
-    public void buildMinHeap(int[] array) {
+    public void buildMaxHeap(Integer[] array) {
         heapify(array);
     }
 
-    public void heapify(int[] array) {
+    public void heapify(Integer[] array) {
         for (int i = (array.length - 1) / 2; i >= 0; i--) {
             siftDown(array, i);
         }
     }
 
-    private void siftDown(int[] array, int index) {
+    private void siftDown(Integer[] array, int index) {
         while (index * 2 + 1 < array.length) {
             int son = index * 2 + 1;
             // get less son
@@ -35,9 +36,47 @@ public class UnsortedArrayConvertMaxHeap {
         }
     }
 
+    public void show(Integer[] array1){
+        int n = array1.length;
+        Integer[] array = new Integer[n+1];
+        if (n%2==0){
+            for (int i=0;i<n+1;++i){
+                if (i<n){
+                    array[i]=array1[i];
+                }
+            }
+        }else {
+            array = array1;
+        }
+        OperationBinaryTree tree = new OperationBinaryTree();
+        tree.addByLeve(array);
+        tree.show(tree.getRoot());
+    }
+
+    public void removeMax(Integer[] array1){
+        int n = array1.length;
+        Integer[] array = new Integer[n-1];
+        for (int i=0;i<n-1;++i){
+            if (i==0){
+                array[i] = array1[n-1];
+            }else {
+                array[i] = array1[i];
+            }
+        }
+        System.out.println("删除后的元素列表排序（未重新堆化）："+JSONObject.toJSONString(array));
+        buildMaxHeap(array);
+        System.out.println("删除后堆顶元素后树的形状：");
+        show(array);
+        System.out.println("删除后堆顶元素排序："+JSONObject.toJSONString(array));
+    }
+
     public static void main(String[] args) {
-        int[] array = {3, 5, 9, 6, 8, 20, 10, 12, 18, 9};
-        new UnsortedArrayConvertMaxHeap().buildMinHeap(array);
-        System.out.println(JSONObject.toJSONString(array));
+        Integer[] array = {3, 5, 9, 6, 8, 20, 10, 12, 18, 9};
+        UnsortedArrayConvertMaxHeap maxHeap = new UnsortedArrayConvertMaxHeap();
+        maxHeap.buildMaxHeap(array);
+        System.out.println("最大堆化后的数组："+JSONObject.toJSONString(array));
+        maxHeap.show(array);
+        System.out.println();
+        maxHeap.removeMax(array);
     }
 }
--- src/main/java/com/twiss/xiaohuang/cp4heap/UnsortedArrayConvertMinHeap.java
+++ src/main/java/com/twiss/xiaohuang/cp4heap/UnsortedArrayConvertMinHeap.java
@@ -1,6 +1,7 @@
 package com.twiss.xiaohuang.cp4heap;
 
 import com.alibaba.fastjson.JSONObject;
+import com.twiss.xiaohuang.util.tree.OperationBinaryTree;
 
 /**
  * @Author: Twiss
@@ -8,17 +9,17 @@ import com.alibaba.fastjson.JSONObject;
  */
 public class UnsortedArrayConvertMinHeap {
 
-    public void buildMinHeap(int[] array) {
+    public void buildMinHeap(Integer[] array) {
         heapify(array);
     }
 
-    public void heapify(int[] array) {
+    public void heapify(Integer[] array) {
         for (int i = (array.length - 1) / 2; i >= 0; i--) {
             siftDown(array, i);
         }
     }
 
-    private void siftDown(int[] array, int index) {
+    private void siftDown(Integer[] array, int index) {
         while (index * 2 + 1 < array.length) {
             int son = index * 2 + 1;
             // get less son
@@ -35,9 +36,47 @@ public class UnsortedArrayConvertMinHeap {
         }
     }
 
+    public void show(Integer[] array1){
+        int n = array1.length;
+        Integer[] array = new Integer[n+1];
+        if (n%2==0){
+            for (int i=0;i<n+1;++i){
+                if (i<n){
+                    array[i]=array1[i];
+                }
+            }
+        }else {
+            array = array1;
+        }
+        OperationBinaryTree tree = new OperationBinaryTree();
+        tree.addByLeve(array);
+        tree.show(tree.getRoot());
+    }
+
+    public void removeMin(Integer[] array1){
+        int n = array1.length;
+        Integer[] array = new Integer[n-1];
+        for (int i=0;i<n-1;++i){
+            if (i==0){
+                array[i] = array1[n-1];
+            }else {
+                array[i] = array1[i];
+            }
+        }
+        System.out.println("删除后的元素列表排序（未重新堆化）："+JSONObject.toJSONString(array));
+        buildMinHeap(array);
+        System.out.println("删除后堆顶元素后树的形状：");
+        show(array);
+        System.out.println("删除后堆顶元素排序："+JSONObject.toJSONString(array));
+    }
+
     public static void main(String[] args) {
-        int[] array = {9, 2, 8, 5, 6, 1, 3};
-        new UnsortedArrayConvertMinHeap().buildMinHeap(array);
-        System.out.println(JSONObject.toJSONString(array));
+        Integer[] array = { 3, 4, 6, 9, 7,2};
+        UnsortedArrayConvertMinHeap minHeap = new UnsortedArrayConvertMinHeap();
+        minHeap.buildMinHeap(array);
+        System.out.println("最小堆化后的数组："+JSONObject.toJSONString(array));
+        minHeap.show(array);
+        System.out.println();
+        minHeap.removeMin(array);
     }
 }
