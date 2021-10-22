4cbf9d509fb784d0f630e9fd6141efe382fcadd7
HQCfly <2249629867@qq.com>
2021-10-22 23:18:25 +0800

    Modified insertion sort list

--- src/main/java/com/twiss/sort/InsertionSortList.java
+++ src/main/java/com/twiss/sort/InsertionSortList.java
@@ -5,4 +5,49 @@ package com.twiss.sort;
  * @Date: 2021/10/19 11:22 下午
  */
 public class InsertionSortList {
+
+    public ListNode insertSort(ListNode node) {
+        if(node==null){
+            return node;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = node;
+        ListNode lastSorted = dummyHead, curr = node.next;
+        while (curr!=null){
+            if (lastSorted.val<=curr.val){
+                lastSorted = lastSorted.next;
+            }else {
+                ListNode pre = dummyHead;
+                while (pre.next.val<=curr.val){
+                    pre = pre.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = pre.next;
+                pre.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
