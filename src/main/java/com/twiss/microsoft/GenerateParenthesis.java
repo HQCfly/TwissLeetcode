7fad3067ca1547e9d11406e1dcda2cd387a0edf8
HQCfly <2249629867@qq.com>
2022-01-29 21:16:33 +0800

    Add generateParenthesis

---
+++ src/main/java/com/twiss/microsoft/GenerateParenthesis.java
@@ -0,0 +1,13 @@
+package com.twiss.microsoft;
+
+/**
+ * 生成括号
+ * @Author: Twiss
+ * @Date: 2022/1/29 9:16 下午
+ */
+public class GenerateParenthesis {
+
+    public static void main(String[] args) {
+
+    }
+}
