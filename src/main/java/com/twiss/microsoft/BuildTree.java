2013168b63110a0ba137800e48c2657ab6fb1c99
HQCfly <2249629867@qq.com>
2022-02-11 23:28:33 +0800

    Modified BuildTree

--- src/main/java/com/twiss/microsoft/BuildTree.java
+++ src/main/java/com/twiss/microsoft/BuildTree.java
@@ -43,6 +43,9 @@ public class BuildTree {
     }
 
     public static void main(String[] args) {
-
+        int[] preorder = {3,9,20,15,7};
+        int[] inorder = {9,3,15,20,7};
+        TreeNode node = new BuildTree().build(preorder,inorder);
+        System.out.println(node.val);
     }
 }
