b78bd6943ff637ef5f456b671a74a16cae9e6849
HQCfly <2249629867@qq.com>
2022-02-22 19:02:09 +0800

    codeformat

--- src/main/java/com/twiss/microsoftII/ReverseString.java
+++ src/main/java/com/twiss/microsoftII/ReverseString.java
@@ -10,44 +10,44 @@ import java.util.*;
  */
 public class ReverseString {
 
-    public String getReverseString(String s){
-        if (s==null){
+    public String getReverseString(String s) {
+        if (s == null) {
             return null;
         }
         // 去掉头部和尾部空格
         s.trim();
         List<String> wordList = Arrays.asList(s.split("//s+"));
         Collections.reverse(wordList);
-        return String.join(" ",wordList);
+        return String.join(" ", wordList);
     }
 
-    public String getReverseStringByDeque(String s){
-        if (s==null){
+    public String getReverseStringByDeque(String s) {
+        if (s == null) {
             return null;
         }
-        int left = 0, right = s.length()-1;
+        int left = 0, right = s.length() - 1;
         //去掉头部空格字符
-        while (left<=right&&s.charAt(left)==' '){
+        while (left <= right && s.charAt(left) == ' ') {
             left++;
         }
         // 去除尾部空格字符
-        while (right>=left&&s.charAt(right)==' '){
+        while (right >= left && s.charAt(right) == ' ') {
             --right;
         }
         Deque<String> deque = new ArrayDeque<>();
         StringBuilder word = new StringBuilder();
-        while (left<=right){
+        while (left <= right) {
             char c = s.charAt(left);
-            if (word.length()!=0&&c==' '){
+            if (word.length() != 0 && c == ' ') {
                 deque.offerFirst(word.toString());
                 word.setLength(0);
-            }else if (c!=' '){
+            } else if (c != ' ') {
                 word.append(c);
             }
             ++left;
         }
         deque.offerFirst(word.toString());
-        return String.join(" ",deque);
+        return String.join(" ", deque);
     }
 
     public static void main(String[] args) {
