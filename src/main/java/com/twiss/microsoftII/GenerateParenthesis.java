1b783d5defd90809d5e77bf0d0e5997d981cb4d6
HQCfly <2249629867@qq.com>
2022-03-01 23:56:12 +0800

    Add Generate Parenthesis

--- src/main/java/com/twiss/microsoftII/GenerateParenthesis.java
+++ src/main/java/com/twiss/microsoftII/GenerateParenthesis.java
@@ -1,20 +1,49 @@
 package com.twiss.microsoftII;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 生成括号
+ * dfs
+ *
  * @Author: Twiss
  * @Date: 2022/2/28 10:26 下午
  */
 public class GenerateParenthesis {
 
-    public String getParenthesis(){
-        StringBuilder res = new StringBuilder();
+    public List<String> getParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        backtrack(ans, stringBuilder, 0, 0, n);
+        return ans;
+    }
 
-
-        return res.toString();
+    public void backtrack(List<String> ans,
+                          StringBuilder stringBuilder,
+                          int open,
+                          int close,
+                          int max) {
+        if (stringBuilder.length() == max * 2) {
+            ans.add(stringBuilder.toString());
+        }
+        if (open<max){
+            stringBuilder.append("(");
+            backtrack(ans,stringBuilder,open+1,close,max);
+            stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        }
+        if (close<open){
+            stringBuilder.append(")");
+            backtrack(ans,stringBuilder,open,close+1,max);
+            stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        }
     }
 
     public static void main(String[] args) {
-
+        int n = 3;
+        List<String> ans = new GenerateParenthesis().getParenthesis(n);
+        System.out.println(JSONObject.toJSONString(ans));
     }
 }
