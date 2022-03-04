804f023bb53699e5863fe05c99434a501f96c17a
HQCfly <2249629867@qq.com>
2022-03-04 23:24:14 +0800

    Add quick sorted2

--- src/main/java/com/twiss/microsoftII/QuickSorted2.java
+++ src/main/java/com/twiss/microsoftII/QuickSorted2.java
@@ -2,8 +2,16 @@ package com.twiss.microsoftII;
 
 /**
  * 快排
+ * 时间复杂度O(nlogn)
  * @Author: Twiss
  * @Date: 2022/3/3 10:11 下午
  */
 public class QuickSorted2 {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
