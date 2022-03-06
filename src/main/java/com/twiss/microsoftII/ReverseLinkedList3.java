7a5b532d9848b44a443fc65e4ce372fde20f6476
HQCfly <2249629867@qq.com>
2022-03-06 16:33:25 +0800

    Add reverse linked list

--- src/main/java/com/twiss/microsoftII/ReverseLinkedList3.java
+++ src/main/java/com/twiss/microsoftII/ReverseLinkedList3.java
@@ -1,5 +1,7 @@
 package com.twiss.microsoftII;
 
+import com.twiss.microsoft.LinkedNode;
+
 /**
  * 反转链表
  * 时间复杂度O(n)
@@ -9,9 +11,26 @@ package com.twiss.microsoftII;
  */
 public class ReverseLinkedList3 {
 
-
+    public LinkedNode getReverse(LinkedNode linkedNode){
+        if (linkedNode==null){
+            return null;
+        }
+        LinkedNode previous = null, tmp = null;
+        LinkedNode curr = linkedNode;
+        while (curr!=null){
+            tmp = curr.next;
+            curr.next = previous;
+            previous = curr;
+            curr = tmp;
+        }
+        return previous;
+    }
 
     public static void main(String[] args) {
-
+        LinkedNode linkedNode = new LinkedNode(1);
+        linkedNode.next = new LinkedNode(2);
+        linkedNode.next.next = new LinkedNode(3);
+        LinkedNode ans = new ReverseLinkedList3().getReverse(linkedNode);
+        System.out.println(ans.val);
     }
 }
