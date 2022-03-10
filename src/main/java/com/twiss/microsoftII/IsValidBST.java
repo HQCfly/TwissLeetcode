a43b5f16372cfefbc57029a133a38e047eb6b6ac
HQCfly <2249629867@qq.com>
2022-03-10 23:03:04 +0800

    Finished isValidBST

--- src/main/java/com/twiss/microsoftII/IsValidBST.java
+++ src/main/java/com/twiss/microsoftII/IsValidBST.java
@@ -2,18 +2,37 @@ package com.twiss.microsoftII;
 
 /**
  * 验证二叉搜索树
+ * 中序遍历
  * 时间复杂度O(n)
  * 空间复杂度O(n)
  * @Author: Twiss
  * @Date: 2022/3/9 11:20 下午
  */
 public class IsValidBST {
+    private Integer pre = Integer.MIN_VALUE;;
 
     public boolean validBst(TreeNode root){
-        return false;
+        if (root==null){
+            return false;
+        }
+        return inorderTraversal(root);
+    }
+
+    private boolean inorderTraversal(TreeNode root){
+        if (root==null){
+            return true;
+        }
+        if (!inorderTraversal(root.left)){
+            return false;
+        }
+        if (pre>=root.val){
+            return false;
+        }
+        pre = root.val;
+        return inorderTraversal(root.right);
     }
 
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode();
     }
 }
