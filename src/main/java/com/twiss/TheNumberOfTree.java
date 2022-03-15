702ab07f45f82809f2f7613c749f81111eac12b2
HQCfly <2249629867@qq.com>
2022-03-15 22:27:08 +0800

    Codeformat

--- src/main/java/com/twiss/TheNumberOfTree.java
+++ src/main/java/com/twiss/TheNumberOfTree.java
@@ -13,28 +13,28 @@ public class TheNumberOfTree {
             return 0;
         }
         int n = arrays.length;
-        int level = getLeve(n)+1;
-        for (int i = 0; i < n/2; ++i) {
-           int newLeft = 2*i+1;
-           int newRight = 2*i+2;
-           if (arrays[i]!=-1&&arrays[newLeft]==-1&&arrays[newRight]==-1){
-               result++;
-           }
-           if (newLeft>=(int) Math.pow(2,level-1)&&(arrays[newLeft]!=-1)){
-               result++;
-           }
-            if (newRight>=(int) Math.pow(2,level-1)&&(arrays[newRight]!=-1)){
+        int level = getLeve(n) + 1;
+        for (int i = 0; i < n / 2; ++i) {
+            int newLeft = 2 * i + 1;
+            int newRight = 2 * i + 2;
+            if (arrays[i] != -1 && arrays[newLeft] == -1 && arrays[newRight] == -1) {
+                result++;
+            }
+            if (newLeft >= (int) Math.pow(2, level - 1) && (arrays[newLeft] != -1)) {
+                result++;
+            }
+            if (newRight >= (int) Math.pow(2, level - 1) && (arrays[newRight] != -1)) {
                 result++;
             }
         }
         return result;
     }
 
-    private int getLeve(int n){
+    private int getLeve(int n) {
         int re = 0;
-        for (int i=0;i<n;++i){
-            re += (int) Math.pow(2,i);
-            if (re>=n){
+        for (int i = 0; i < n; ++i) {
+            re += (int) Math.pow(2, i);
+            if (re >= n) {
                 return i;
             }
         }
@@ -42,7 +42,7 @@ public class TheNumberOfTree {
     }
 
     public static void main(String[] args) {
-        int[] arrays = {1, 2, 3, -1, 4, -5, 6,-1, -1, 7, 8, 9, -1, -1, 10};
+        int[] arrays = {1, 2, 3, -1, 4, -5, 6, -1, -1, 7, 8, 9, -1, -1, 10};
         int ans = new TheNumberOfTree().getNumber(arrays);
         System.out.println(ans);
     }
