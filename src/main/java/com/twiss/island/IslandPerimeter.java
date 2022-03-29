19842c8cf3ffa130253d71ba630602103527de76
HQCfly <2249629867@qq.com>
2022-03-29 17:04:21 +0800

    Add IslandPerimeter2

--- src/main/java/com/twiss/island/IslandPerimeter.java
+++ src/main/java/com/twiss/island/IslandPerimeter.java
@@ -6,23 +6,23 @@ package com.twiss.island;
  */
 public class IslandPerimeter {
 
-    public int getPerimeter(int[][] grids){
-        for (int i=0;i<grids.length;++i){
-            for (int j=0;j<grids[0].length;++j){
-                if (grids[i][j]==1){
-                    return dfs(grids,i,j);
+    public int getPerimeter(int[][] grids) {
+        for (int i = 0; i < grids.length; ++i) {
+            for (int j = 0; j < grids[0].length; ++j) {
+                if (grids[i][j] == 1) {
+                    return dfs(grids, i, j);
                 }
             }
         }
         return 0;
     }
 
-    private int dfs(int[][] grids, int i, int j){
-        if (!inArea(grids,i,j)||grids[i][j]==0){
+    private int dfs(int[][] grids, int i, int j) {
+        if (!inArea(grids, i, j) || grids[i][j] == 0) {
             return 1;
         }
 
-        if (grids[i][j]!=1){
+        if (grids[i][j] != 1) {
             return 0;
         }
 
@@ -33,17 +33,17 @@ public class IslandPerimeter {
                 + dfs(grids, i, j + 1);
     }
 
-    private boolean inArea(int[][] grids, int i,int j){
-        return 0<=i&&i<grids.length&&
-                0<=j&&j<grids[0].length;
+    private boolean inArea(int[][] grids, int i, int j) {
+        return 0 <= i && i < grids.length &&
+                0 <= j && j < grids[0].length;
     }
 
     public static void main(String[] args) {
         int[][] grids = {
-                {1,1,1,1,0},
-                {1,1,0,0,0},
-                {1,1,0,1,0},
-                {0,1,0,0,0},
+                {1, 1, 1, 1, 0},
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 0},
+                {0, 1, 0, 0, 0},
         };
         int res = new IslandPerimeter().getPerimeter(grids);
         System.out.println(res);
--- src/main/java/com/twiss/island/IslandPerimeter2.java
+++ src/main/java/com/twiss/island/IslandPerimeter2.java
@@ -7,4 +7,45 @@ package com.twiss.island;
  * @Date: 2022/3/15 6:19 下午
  */
 public class IslandPerimeter2 {
+
+    public int getPerimeter(int[][] grids) {
+        for (int i = 0; i < grids.length; ++i) {
+            for (int j = 0; j < grids[0].length; ++j) {
+                if (grids[i][j] == 1) {
+                    return dfs(grids, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int dfs(int[][] grids, int i, int j) {
+        if (!inArea(grids, i, j) || grids[i][j] == 0) {
+            return 1;
+        }
+        if (grids[i][j] != 1) {
+            return 0;
+        }
+        grids[i][j] = 2;
+        return dfs(grids, i - 1, j) +
+                dfs(grids, i + 1, j) +
+                dfs(grids, i, j - 1) +
+                dfs(grids, i, j + 1);
+    }
+
+    private boolean inArea(int[][] grids, int i, int j) {
+        return 0 <= i && i < grids.length &&
+                0 <= j && j < grids[0].length;
+    }
+
+    public static void main(String[] args) {
+        int[][] grids = {
+                {1, 1, 1, 1, 0},
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 0},
+                {0, 1, 0, 0, 0},
+        };
+        int ans = new IslandPerimeter2().getPerimeter(grids);
+        System.out.println(ans);
+    }
 }
