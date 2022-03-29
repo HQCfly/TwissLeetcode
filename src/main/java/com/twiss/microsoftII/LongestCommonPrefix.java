978126afd3db773df8a421d0574039122412882e
HQCfly <2249629867@qq.com>
2022-03-29 17:06:08 +0800

    Add LongestCommonPrefix

---
+++ src/main/java/com/twiss/microsoftII/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+package com.twiss.microsoftII;
+
+/**
+ * 最长公共前缀
+ * 时间复杂度O(mn)
+ * 空间复杂度O(1)
+ * https://leetcode-cn.com/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
+ * @Author: Twiss
+ * @Date: 2022/3/29 5:05 下午
+ */
+public class LongestCommonPrefix {
+}
