7246f93a5b64626308845c0f8798659780e7d7c7
HQCfly <2249629867@qq.com>
2022-04-02 20:39:34 +0800

    Add two sum

---
+++ src/main/java/com/twiss/zijie/TwoSum.java
@@ -0,0 +1,22 @@
+package com.twiss.zijie;
+
+/**
+ * 两数和
+ * 时间复杂度O(N)
+ * 空间复杂度O(N)
+ * @Author: Twiss
+ * @Date: 2022/4/2 8:29 下午
+ */
+public class TwoSum {
+
+    public int getSum(int[] nums){
+        if (nums==null){
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
