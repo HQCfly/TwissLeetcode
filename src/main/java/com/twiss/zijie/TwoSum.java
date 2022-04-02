a79987804a5fdc26b7a7de2ff2ec0ce92e099461
HQCfly <2249629867@qq.com>
2022-04-02 20:46:21 +0800

    Add two sum

--- src/main/java/com/twiss/zijie/TwoSum.java
+++ src/main/java/com/twiss/zijie/TwoSum.java
@@ -1,22 +1,39 @@
 package com.twiss.zijie;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+
 /**
  * 两数和
  * 时间复杂度O(N)
  * 空间复杂度O(N)
+ *
  * @Author: Twiss
  * @Date: 2022/4/2 8:29 下午
  */
 public class TwoSum {
 
-    public int getSum(int[] nums){
-        if (nums==null){
-
+    public int[] getSum(int[] nums,int target) {
+        if (nums == null) {
+            return null;
         }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i=0;i<nums.length;++i){
+            int tmp = target-nums[i];
+            if (map.containsKey(tmp)){
+                return new int[]{i,map.get(tmp)};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] ans = new TwoSum().getSum(nums,target);
+        System.out.println(JSONObject.toJSONString(ans));
     }
 
 }
