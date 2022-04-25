750cdfbe8777aa7b9b303298f192d5c1053a7fee
HQCfly <2249629867@qq.com>
2022-04-25 19:31:29 +0800

    Add the number of is land

--- src/main/java/com/twiss/zijie/TheNumberOfIslands.java
+++ src/main/java/com/twiss/zijie/TheNumberOfIslands.java
@@ -7,7 +7,45 @@ package com.twiss.zijie;
  */
 public class TheNumberOfIslands {
 
-    public static void main(String[] args) {
+    public int getNumber(int[][] grids){
+        int res = 0;
+        for (int i=0;i<grids.length;++i){
+            for (int j=0;j<grids[0].length;++j){
+                if (grids[i][j]==1){
+                    dfs(grids,i,j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
 
+    private void dfs(int[][] grids, int i,int j){
+        if (!isArea(grids,i,j)){
+            return;
+        }
+        if (grids[i][j]!=1){
+            return;
+        }
+        grids[i][j]=2;
+        dfs(grids,i-1,j);
+        dfs(grids,i+1,j);
+        dfs(grids,i,j-1);
+        dfs(grids,i,j+1);
+    }
+
+    private boolean isArea(int[][] grids,int i,int j){
+        return i>=0&&i<grids.length&&j>=0&&j<grids[0].length;
+    }
+
+    public static void main(String[] args) {
+        int[][] grids = {
+                {1,1,1,1,0},
+                {1,1,0,1,0},
+                {1,1,0,0,0},
+                {0,0,0,1,0},
+        };
+        int res = new TheNumberOfIslands().getNumber(grids);
+        System.out.println(res);
     }
 }
