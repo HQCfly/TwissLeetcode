7b65a41724739f7b19a7e784d05c8705c903b39f
HQCfly <2249629867@qq.com>
2022-05-04 20:45:14 +0800

    Add complete circuit

--- src/main/java/com/twiss/greed/CompleteCircuit.java
+++ src/main/java/com/twiss/greed/CompleteCircuit.java
@@ -8,4 +8,60 @@ package com.twiss.greed;
  * @Date: 2022/5/3 10:39 下午
  */
 public class CompleteCircuit {
+
+    /**
+     * 1、如果sum为<0, return -1
+     *
+     * @param gas
+     * @param cost
+     * @return
+     */
+    public int  getIndex(int[] gas,int[] cost){
+        int minSum = 0, sum=0;
+        for (int i=0;i<gas.length;++i){
+            sum += (gas[i]-cost[i]);
+            minSum = Math.min(minSum,sum);
+        }
+
+        if (sum<0){
+            return -1;
+        }
+        if (minSum>=0){
+            return 0;
+        }
+        for (int i=gas.length-1;i>=0;i--){
+            minSum+=(gas[i]-cost[i]);
+            if (minSum>=0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getIndexByGreed(int[] gas,int[] cost){
+        int currSum = 0, sum = 0;
+        int index = 0;
+        for (int i=0;i<gas.length;i++){
+            currSum+=(gas[i]-cost[i]);
+            sum+=(gas[i]-cost[i]);
+            if (currSum<0){
+                index = (i+1)%gas.length;
+                currSum = 0;
+            }
+        }
+        if (sum<0){
+            return -1;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        int index = new CompleteCircuit().getIndex(gas,cost);
+        System.out.println(index);
+
+        int index2 = new CompleteCircuit().getIndexByGreed(gas,cost);
+        System.out.println(index2);
+    }
 }
