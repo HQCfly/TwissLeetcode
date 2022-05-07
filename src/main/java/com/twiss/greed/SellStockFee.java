d23f8c1655a5ca1635962df872b030d08edc0fc6
HQCfly <2249629867@qq.com>
2022-05-07 23:38:14 +0800

    Add sellstock

---
+++ src/main/java/com/twiss/greed/SellStockFee.java
@@ -0,0 +1,29 @@
+package com.twiss.greed;
+
+/**
+ * 买卖股票含有手续费
+ * 时间复杂度O()
+ * 空间复杂度O()
+ * @Author: Twiss
+ * @Date: 2022/5/7 11:34 下午
+ */
+public class SellStockFee {
+
+    public int getMaxProfit(int[] stocks,int fee){
+        int buy = stocks[0]+fee;
+        int sum = 0;
+        for (int price:stocks){
+            if (price+fee<buy){
+                buy = price+fee;
+            }else if (price<buy){
+                sum+=price-buy;
+                buy = price;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
