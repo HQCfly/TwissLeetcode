b7e910909ac747abc0113a89f71c50571ac8fc89
HQCfly <2249629867@qq.com>
2022-05-20 13:38:01 +0800

    Modified max length

--- src/main/java/com/twiss/dynamic/substring/MaxLengthIncreaseSubstring.java
+++ src/main/java/com/twiss/dynamic/substring/MaxLengthIncreaseSubstring.java
@@ -16,24 +16,26 @@ public class MaxLengthIncreaseSubstring {
         }
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,1);
-        for (int i=0;i<n;i++){
-            for (int j=0;j<i;j++){
-                if (nums[i]>nums[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
         int res = 0;
-        for (int i=0;i<n;i++){
-            res = Math.max(res,dp[i]);
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
         }
         return res;
     }
 
+
     public static void main(String[] args) {
-        int[] nums ={10,9,2,5,3,7,101,18};
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
         int ans = new MaxLengthIncreaseSubstring().getMaxLength(nums);
         System.out.println(ans);
+
     }
 }
