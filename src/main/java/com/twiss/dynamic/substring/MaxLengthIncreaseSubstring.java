a5d5970fca8bf6ebdc0e997cb4985a3cf6d9192f
HQCfly <2249629867@qq.com>
2022-05-20 13:30:11 +0800

    Add max length increse substring

---
+++ src/main/java/com/twiss/dynamic/substring/MaxLengthIncreaseSubstring.java
@@ -0,0 +1,39 @@
+package com.twiss.dynamic.substring;
+
+import java.util.Arrays;
+
+/**
+ * 最长上升子序列
+ *
+ * @Author: Twiss
+ * @Date: 2022/5/20 1:21 下午
+ */
+public class MaxLengthIncreaseSubstring {
+
+    public int getMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                if (nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        int res = 0;
+        for (int i=0;i<n;i++){
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={10,9,2,5,3,7,101,18};
+        int ans = new MaxLengthIncreaseSubstring().getMaxLength(nums);
+        System.out.println(ans);
+    }
+}
