a8fcd2f40ee6eba380502998084b4e6d3e584328
HQCfly <2249629867@qq.com>
2022-05-21 13:45:10 +0800

    Add MaxLengthSubstringDuplicate

---
+++ src/main/java/com/twiss/dynamic/substring/MaxLengthSubstringDuplicate.java
@@ -0,0 +1,32 @@
+package com.twiss.dynamic.substring;
+
+/**
+ * 最长重复子数组
+ * @Author: Twiss
+ * @Date: 2022/5/21 1:34 下午
+ */
+public class MaxLengthSubstringDuplicate {
+
+    public int getMax(int[] num1,int[] num2){
+        int n = num1.length+1, m = num2.length+1;
+        int[][] dp = new int[n][m];
+
+        int res = 0;
+        for (int i=1;i<n;i++){
+            for (int j=1;j<m;j++){
+                if (num1[i-1]==num2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    res = Math.max(dp[i][j],res);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] num1 = {1,2,3,2,1};
+        int[] num2 = {3,2,1,4,7};
+        int ans = new MaxLengthSubstringDuplicate().getMax(num1,num2);
+        System.out.println(ans);
+    }
+}
