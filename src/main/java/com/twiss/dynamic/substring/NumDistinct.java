2102d4f2a8ec2259846f40e7d2b8ce3ab2eeb4ff
HQCfly <2249629867@qq.com>
2022-05-23 14:39:13 +0800

    Modified code format

--- src/main/java/com/twiss/dynamic/substring/NumDistinct.java
+++ src/main/java/com/twiss/dynamic/substring/NumDistinct.java
@@ -2,23 +2,24 @@ package com.twiss.dynamic.substring;
 
 /**
  * 不同子序列
+ *
  * @Author: Twiss
  * @Date: 2022/5/23 2:30 下午
  */
 public class NumDistinct {
 
-    public int getDistinct(String s, String t){
+    public int getDistinct(String s, String t) {
         int m = s.length(), n = t.length();
-        int[][] dp = new int[m+1][n+1];
+        int[][] dp = new int[m + 1][n + 1];
         for (int i = 0; i < m + 1; i++) {
             dp[i][0] = 1;
         }
-        for (int i=1;i<=m;i++){
-            for (int j=1;j<=n;j++){
-                if (s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-                }else {
-                    dp[i][j] = dp[i-1][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
@@ -28,7 +29,7 @@ public class NumDistinct {
     public static void main(String[] args) {
         String s = "babgbag";
         String t = "bag";
-        int ans = new NumDistinct().getDistinct(s,t);
+        int ans = new NumDistinct().getDistinct(s, t);
         System.out.println(ans);
     }
 }
