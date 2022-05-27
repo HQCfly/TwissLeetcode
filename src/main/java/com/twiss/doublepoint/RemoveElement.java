23972fd446a5e73dbb60b8de3f2ed158820f9c86
HQCfly <2249629867@qq.com>
2022-05-27 11:28:26 +0800

    Modfied remove element

--- src/main/java/com/twiss/doublepoint/RemoveElement.java
+++ src/main/java/com/twiss/doublepoint/RemoveElement.java
@@ -3,6 +3,7 @@ package com.twiss.doublepoint;
 /**
  * 移除元素
  * 不能使用额外空间，可以改变元素位置
+ * 双指针
  * @Author: Twiss
  * @Date: 2022/5/27 11:06 上午
  */
@@ -24,10 +25,27 @@ public class RemoveElement {
         return left;
     }
 
+    public int getNewArraySize(int[] nums, int val){
+        if (nums==null||nums.length==0){
+            return 0;
+        }
+        int slow = 0, fast;
+        for (fast=0;fast<nums.length;fast++){
+            if (nums[fast]!=val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+
     public static void main(String[] args) {
         int[] array = {0,1,2,3,3,0,4,2};
         int tar = 2;
         int size = new RemoveElement().getNewArray(array,tar);
         System.out.println(size);
+        int[] array2 = {0,1,2,3,3,0,4,2};
+        int size2 = new RemoveElement().getNewArraySize(array2,tar);
+        System.out.println(size2);
     }
 }
