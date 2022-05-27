752fcdbffe20c7bb058bf07f0e89bf53653748cc
HQCfly <2249629867@qq.com>
2022-05-27 15:03:37 +0800

    Add back space compare

---
+++ src/main/java/com/twiss/doublepoint/BackspaceCompare.java
@@ -0,0 +1,57 @@
+package com.twiss.doublepoint;
+
+/**
+ * @Author: Twiss
+ * @Date: 2022/5/27 2:47 下午
+ */
+public class BackspaceCompare {
+
+    public boolean isValid(String S, String T){
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            } else {
+                if (i >= 0 || j >= 0) {
+                    return false;
+                }
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "ab#c";
+        String t = "ad#c";
+        boolean ans = new BackspaceCompare().isValid(s,t);
+        System.out.println(ans);
+    }
+}
