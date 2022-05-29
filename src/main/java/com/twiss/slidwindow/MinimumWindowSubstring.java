67ab51ecba3f2c353f7354c8ac23ae18d6be4876
HQCfly <2249629867@qq.com>
2022-05-29 13:31:30 +0800

    Modified MinimumWindowSubstring

--- src/main/java/com/twiss/slidwindow/MinimumWindowSubstring.java
+++ src/main/java/com/twiss/slidwindow/MinimumWindowSubstring.java
@@ -1,6 +1,7 @@
 package com.twiss.slidwindow;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 最小覆盖子串
@@ -43,10 +44,49 @@ public class MinimumWindowSubstring {
         return ans;
     }
 
+    public String getMinimumWindowII(String s, String t){
+        Map<Character, Integer> hit = new HashMap<>();
+        Map<Character,Integer> window = new HashMap<>();
+        // 初始化hit
+        for (int i=0;i<t.length();i++){
+            Character c = t.charAt(i);
+            hit.put(c,hit.getOrDefault(c,0)+1);
+        }
+        String ans = "";
+        // 最小重复子串长度
+        int len = Integer.MAX_VALUE;
+        // hit包含目标串长度，在s中的
+        int cnt = 0;
+        for (int start=0,end =0;end<s.length();end++){
+            Character c = s.charAt(end);
+            // 添加元素到window中
+            window.put(c,window.getOrDefault(c,0)+1);
+            // 判断并增加cnt
+            if (hit.containsKey(c)&&window.get(c)<=hit.get(c)){
+                cnt++;
+            }
+            // 收缩start指针
+            while (start<end&&(!hit.containsKey(s.charAt(start)))||(hit.get(s.charAt(start))<window.get(s.charAt(start)))){
+                window.put(s.charAt(start),window.get(s.charAt(start))-1);
+                start++;
+            }
+            // 判断最小覆盖子串
+            if (cnt==t.length()&&end-start+1<len){
+                len = end-start+1;
+                ans = s.substring(start,end+1);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
         String t = "ABC";
         String ans = new MinimumWindowSubstring().getMinimumWindow(s,t);
         System.out.println(ans);
+        String s2 = "ADOBECODEBANC";
+        String t2 = "ABC";
+        String ans2 = new MinimumWindowSubstring().getMinimumWindowII(s2,t2);
+        System.out.println(ans2);
     }
 }
