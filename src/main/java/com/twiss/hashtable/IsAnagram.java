b13141eaeb91ba79e42fc01d273daf1867336658
HQCfly <2249629867@qq.com>
2022-06-02 10:17:24 +0800

    Add is anagram

--- src/main/java/com/twiss/hashtable/IsAnagram.java
+++ src/main/java/com/twiss/hashtable/IsAnagram.java
@@ -1,8 +1,32 @@
 package com.twiss.hashtable;
 
 /**
+ * 有效的字母异位词
  * @Author: Twiss
  * @Date: 2022/6/1 11:59 下午
  */
 public class IsAnagram {
+
+    public boolean isvalid(String s, String t){
+        int[] record = new int[26];
+        for (int i=0;i<s.length();i++){
+            record[s.charAt(i)-'a'] +=1;
+        }
+        for (int i=0;i<t.length();i++){
+            record[t.charAt(i)-'a'] -=1;
+        }
+        for (int i:record){
+            if (i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        boolean ans = new IsAnagram().isvalid(s,t);
+        System.out.println(ans);
+    }
 }
