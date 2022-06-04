aa2cac52f57a2a844ab8a56f452d9abcb35f1b23
HQCfly <2249629867@qq.com>
2022-06-04 17:45:13 +0800

    Add four sum

---
+++ src/main/java/com/twiss/hashtable/FourSum.java
@@ -0,0 +1,60 @@
+package com.twiss.hashtable;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 三数之和
+ * @Author: Twiss
+ * @Date: 2022/6/4 3:49 下午
+ */
+public class FourSum {
+
+    public List<List<Integer>> getNums(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (j > i + 1 && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (right > left) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum > 0) {
+                        right--;
+                    } else if (sum < 0) {
+                        left++;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (right > left && nums[left] == nums[left + 1]) left++;
+
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        List<List<Integer>> ans = new FourSum().getNums(nums);
+        System.out.println(JSONObject.toJSONString(ans));
+    }
+}
