745a438e39f2aa6d5531541078a0a9bd5db3d60f
HQCfly <2249629867@qq.com>
2022-06-06 15:18:23 +0800

    Add longest common prefix

---
+++ src/main/java/com/twiss/amazon/LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+package com.twiss.amazon;
+
+/**
+ * 最长公共前缀
+ * @Author: Twiss
+ * @Date: 2022/6/6 3:11 下午
+ */
+public class LongestCommonPrefix {
+
+    public String getCommonPrefix(String[] strings){
+        if (strings==null||strings.length==0){
+            return null;
+        }
+        String prefix = strings[0];
+        int count = strings.length;
+        for (int i=1;i<count;i++){
+            prefix = commonPrefix(prefix,strings[i]);
+            if (prefix.length()==0){
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    private String commonPrefix(String s1,String s2){
+        int length = Math.min(s1.length(),s2.length());
+        int index = 0;
+        while (index<length&&s1.charAt(index)==s2.charAt(index)){
+            index++;
+        }
+        return s1.substring(0,index);
+    }
+
+    public static void main(String[] args) {
+        String[] s = {
+                "flower",
+                "flow",
+                "flight"
+        };
+        String ans = new LongestCommonPrefix().getCommonPrefix(s);
+        System.out.println(ans);
+    }
+}
