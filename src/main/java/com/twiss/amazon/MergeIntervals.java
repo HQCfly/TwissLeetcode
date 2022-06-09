e0ba7cf4368f00e768ef1292d346a669b0dcb983
HQCfly <2249629867@qq.com>
2022-06-09 23:31:23 +0800

    Modified merge intervals

--- src/main/java/com/twiss/amazon/MergeIntervals.java
+++ src/main/java/com/twiss/amazon/MergeIntervals.java
@@ -9,36 +9,37 @@ import java.util.List;
 
 /**
  * 合并区间
+ *
  * @Author: Twiss
  * @Date: 2022/6/6 11:01 上午
  */
 public class MergeIntervals {
 
-    public int[][] getNewIntervals(int[][] intervals){
-        if (intervals==null||intervals.length==0){
+    public int[][] getNewIntervals(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
             return null;
         }
-        Arrays.sort(intervals,(o1,o2)->Integer.compare(o1[0],o2[0]));
+        Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
         List<int[]> res = new ArrayList<>();
         int start = intervals[0][0];
-        for (int i=1;i<intervals.length;i++){
-            if (intervals[i][0]>intervals[i-1][1]){
-                res.add(new int[]{start,intervals[i-1][1]});
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > intervals[i - 1][1]) {
+                res.add(new int[]{start, intervals[i - 1][1]});
                 start = intervals[i][0];
-            }else {
-                intervals[i][1] = Math.max(intervals[i][1],intervals[i-1][1]);
+            } else {
+                intervals[i][1] = Math.max(intervals[i][1], intervals[i - 1][1]);
             }
         }
-        res.add(new int[]{start,intervals[intervals.length-1][1]});
+        res.add(new int[]{start, intervals[intervals.length - 1][1]});
         return res.toArray(new int[res.size()][]);
     }
 
     public static void main(String[] args) {
         int[][] intervals = {
-                {1,3},
-                {2,6},
-                {8,10},
-                {15,18}
+                {1, 3},
+                {2, 6},
+                {8, 10},
+                {15, 18}
         };
         int[][] ans = new MergeIntervals().getNewIntervals(intervals);
         System.out.println(JSONObject.toJSONString(ans));
