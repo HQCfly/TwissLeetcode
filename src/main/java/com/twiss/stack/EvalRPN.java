497795896d4ce6e3b0c49f9e8cbf88038e5968c1
HQCfly <2249629867@qq.com>
2022-06-13 22:35:20 +0800

    Modified codeformat

--- src/main/java/com/twiss/stack/EvalRPN.java
+++ src/main/java/com/twiss/stack/EvalRPN.java
@@ -5,29 +5,30 @@ import java.util.LinkedList;
 
 /**
  * 逆波兰表达式求值
+ *
  * @Author: Twiss
  * @Date: 2022/6/13 10:16 下午
  */
 public class EvalRPN {
 
-    public int getResult(String[] tokens){
-        if (tokens==null||tokens.length==0){
+    public int getResult(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
             return 0;
         }
         Deque<Integer> stack = new LinkedList<>();
-        for (int i=0;i<tokens.length;i++){
+        for (int i = 0; i < tokens.length; i++) {
             String tmp = tokens[i].trim();
-            if ("+".equals(tmp)){
-                stack.push(stack.pop()+stack.pop());
-            }else if ("-".equals(tmp)){
-                stack.push(-stack.pop()+stack.pop());
-            }else if ("*".equals(tmp)){
-                stack.push(stack.pop()*stack.pop());
-            }else if ("/".equals(tmp)){
+            if ("+".equals(tmp)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("-".equals(tmp)) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if ("*".equals(tmp)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(tmp)) {
                 Integer num1 = stack.pop();
                 Integer num2 = stack.pop();
-                stack.push(num2/num1);
-            }else {
+                stack.push(num2 / num1);
+            } else {
                 stack.push(Integer.valueOf(tokens[i]));
             }
         }
