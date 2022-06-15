bd557a7e221b096e2c1170c877606117de10d142
HQCfly <2249629867@qq.com>
2022-06-15 21:58:11 +0800

    Add top k

---
+++ src/main/java/com/twiss/queue/TopFrequent.java
@@ -0,0 +1,20 @@
+package com.twiss.queue;
+
+/**
+ * 前k个高频元素
+ * @Author: Twiss
+ * @Date: 2022/6/15 9:53 下午
+ */
+public class TopFrequent {
+
+    public int[] getFrequent(int[] nums,int k){
+        if (nums==null||nums.length==0){
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
