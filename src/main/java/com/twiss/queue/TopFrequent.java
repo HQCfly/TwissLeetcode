690466170fd8910ba24609f8b8acc8b82bdca5f3
HQCfly <2249629867@qq.com>
2022-06-16 10:19:17 +0800

    Modified top frequent

--- src/main/java/com/twiss/queue/TopFrequent.java
+++ src/main/java/com/twiss/queue/TopFrequent.java
@@ -1,5 +1,12 @@
 package com.twiss.queue;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  * 前k个高频元素
  * @Author: Twiss
@@ -11,10 +18,31 @@ public class TopFrequent {
         if (nums==null||nums.length==0){
             return null;
         }
-
+        int[] res = new int[k];
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for (int num : nums) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+        Set<Map.Entry<Integer,Integer>> entries = hashMap.entrySet();
+        PriorityQueue<Map.Entry<Integer,Integer>> queue = new PriorityQueue<>((o1,o2)->o1.getValue()-o2.getValue());
+        // 遍历全部的元素，维持元素为k的最小堆。先构建一个k+1的最小堆，可以确定当前堆中最小值，然后将堆顶元素移除即可。
+        for (Map.Entry<Integer,Integer> entry:entries){
+            System.out.println(JSONObject.toJSONString(entry));
+            queue.offer(entry);
+            if (queue.size()>k){
+                queue.poll();
+            }
+        }
+        for (int i=k-1;i>=0;i--){
+            res[i] = queue.poll().getKey();
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {1,1,1,2,2,3,3,3,3,4};
+        int k = 2;
+        int[] ans = new TopFrequent().getFrequent(nums,k);
+        System.out.println(JSONObject.toJSONString(ans));
     }
 }
