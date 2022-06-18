4fd78178f36c735437e6ad25dffd759f48a67b4a
HQCfly <2249629867@qq.com>
2022-06-18 16:38:47 +0800

    Add add right point

---
+++ src/main/java/com/twiss/tree/AddRightPoint.java
@@ -0,0 +1,53 @@
+package com.twiss.tree;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 填充每个节点的下一个右侧节点指针
+ * @Author: Twiss
+ * @Date: 2022/6/18 3:56 下午
+ */
+public class AddRightPoint {
+
+    public List<String> getLevel(TreeNode root){
+        List<String> res = new ArrayList<>();
+        if (root==null){
+            return res;
+        }
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(root);
+        while (!deque.isEmpty()){
+            int size = deque.size();
+            for (int i=0;i<size;i++){
+                TreeNode node = deque.poll();
+                if (i<size-1){
+                    node.next = deque.peek();
+                }
+                if (node.left!=null){
+                    deque.offer(node.left);
+                }
+                if (node.right!=null){
+                    deque.offer(node.right);
+                }
+                res.add(String.valueOf(node.val));
+                if (i==size-1){
+                    res.add("#");
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        List<String> ans = new AddRightPoint().getLevel(root);
+        System.out.println(JSONObject.toJSONString(ans));
+    }
+}
