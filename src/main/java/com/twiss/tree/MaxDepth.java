e57eb2c120c93f6e0cbd81ae2d8646ee06e5e924
HQCfly <2249629867@qq.com>
2022-06-18 17:28:33 +0800

    Add tree serialize

--- src/main/java/com/twiss/tree/MaxDepth.java
+++ src/main/java/com/twiss/tree/MaxDepth.java
@@ -1,13 +1,38 @@
 package com.twiss.tree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 二叉树最大深度
+ *
  * @Author: Twiss
  * @Date: 2022/6/18 4:51 下午
  */
 public class MaxDepth {
 
-
+    public int getMaxDepth(TreeNode root) {
+        int depth = 0;
+        if (root == null) {
+            return depth;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                if (node.left!=null){
+                    queue.offer(node.left);
+                }
+                if (node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
 
     public static void main(String[] args) {
 
