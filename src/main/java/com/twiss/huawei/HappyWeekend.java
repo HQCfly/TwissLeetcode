0bc2ad316f403b1efce7a3c65293253ffd3eb262
HQCfly <2249629867@qq.com>
2022-06-27 23:15:46 +0800

    Add happy weekend

---
+++ src/main/java/com/twiss/huawei/HappyWeekend.java
@@ -0,0 +1,14 @@
+package com.twiss.huawei;
+
+/**
+ * 欢乐周末
+ * @Author: Twiss
+ * @Date: 2022/6/27 11:14 下午
+ */
+public class HappyWeekend {
+
+
+    public static void main(String[] args) {
+
+    }
+}
