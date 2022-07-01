a3b864a0592a30d72718ed4a241bf19627905798
HQCfly <2249629867@qq.com>
2022-07-01 21:21:02 +0800

    Add Candy

--- src/main/java/com/twiss/weilai/Candy.java
+++ src/main/java/com/twiss/weilai/Candy.java
@@ -14,18 +14,14 @@ public class Candy {
             int limit = sc.nextInt();
             sc.nextLine();
             String[] arr = sc.nextLine().split(" ");
-            int[] values = new int[n];
+            int[] weight = new int[n];
             for (int i = 0; i < n; i++) {
-                values[i] = Integer.parseInt(arr[i]);
+                weight[i] = Integer.parseInt(arr[i]);
             }
             int[] dp = new int[limit + 1];
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= limit; j++) {
-                    if (i > 0 && values[i-1] <= j) {
-                        dp[j] = Math.max(dp[j], dp[j - values[i-1]] + 1);
-                    } else {
-                        dp[j] = dp[j-1];
-                    }
+            for (int i = 0; i < n; i++) {
+                for (int j = limit; j >=weight[i]; j--) {
+                    dp[j] = Math.max(dp[j],dp[j-weight[i]]+1);
                 }
             }
             System.out.println(dp[limit]);
