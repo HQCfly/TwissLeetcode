bd46965c3f7ecc9b2fe4fe3ea55ce9b3bf9b5632
HQCfly <2249629867@qq.com>
2022-07-09 23:12:09 +0800

    Modifed dijkstra

--- src/main/java/com/twiss/huawei/Dijkstra.java
+++ src/main/java/com/twiss/huawei/Dijkstra.java
@@ -12,8 +12,8 @@ public class Dijkstra {
 
     private final int INF = Integer.MAX_VALUE;
 
-    public int getMinDistance(int n,int[][] matrix, int vs, int end) {
-        if (n==0||matrix==null||matrix.length==0){
+    public int getMinDistance(int n, int[][] matrix, int vs, int end) {
+        if (n == 0 || matrix == null || matrix.length == 0) {
             return 0;
         }
         // 表示"顶点vs"到"顶点i"的最短路径已成功获
@@ -23,7 +23,7 @@ public class Dijkstra {
         // 起点到i节点的距离表
         int[] dis = new int[n];
         // 初始化
-        for (int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             flag[i] = false;
             prev[i] = 0;
             dis[i] = matrix[vs][i];
@@ -33,20 +33,20 @@ public class Dijkstra {
 
         // 遍历n次；每次找出一个顶点的最短路径。
         int k = 0;
-        for (int i=1;i<n;i++){
+        for (int i = 1; i < n; i++) {
             // 第一次寻找最短距离节点
             int min = INF;
-            for (int j=0;j<n;j++){
-                if (!flag[j] &&dis[j]<min){
+            for (int j = 0; j < n; j++) {
+                if (!flag[j] && dis[j] < min) {
                     min = dis[j];
                     k = j;
                 }
             }
             flag[k] = true;
             // 修正U集合里面的顶点距离
-            for (int j=0;j<n;j++){
-                int tmp = matrix[k][j]==INF?INF:(min+matrix[k][j]);
-                if (!flag[j]&&tmp<dis[j]){
+            for (int j = 0; j < n; j++) {
+                int tmp = matrix[k][j] == INF ? INF : (min + matrix[k][j]);
+                if (!flag[j] && tmp < dis[j]) {
                     dis[j] = tmp;
                     prev[j] = k;
                 }
@@ -57,18 +57,18 @@ public class Dijkstra {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()){
+        while (sc.hasNext()) {
             int n = sc.nextInt();
             int m = sc.nextInt();
             int[][] matrix = new int[n][n];
-            for (int i=0;i<m;i++){
+            for (int i = 0; i < m; i++) {
                 int row = sc.nextInt();
                 int col = sc.nextInt();
-                matrix[col-1][row-1] = matrix[row-1][col-1] = sc.nextInt();
+                matrix[col - 1][row - 1] = matrix[row - 1][col - 1] = sc.nextInt();
             }
             int vs = sc.nextInt();
             int end = sc.nextInt();
-            int ans = new Dijkstra().getMinDistance(n,matrix,vs-1,end-1);
+            int ans = new Dijkstra().getMinDistance(n, matrix, vs - 1, end - 1);
             System.out.println(ans);
         }
     }
