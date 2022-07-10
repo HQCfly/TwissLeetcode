45a8da0ec04616ca7780e457e181433d651513c1
HQCfly <2249629867@qq.com>
2022-07-10 22:22:30 +0800

    Modfied find strong people

--- src/main/java/com/twiss/huawei/FindStrongPeople.java
+++ src/main/java/com/twiss/huawei/FindStrongPeople.java
@@ -6,30 +6,31 @@ import java.util.*;
 
 /**
  * 找到比自己强的人
+ *
  * @Author: Twiss
  * @Date: 2022/6/21 10:34 下午
  */
 public class FindStrongPeople {
 
-    public int[] getStrongPeople(int[][] people){
-        if (people==null||people.length==0){
+    public int[] getStrongPeople(int[][] people) {
+        if (people == null || people.length == 0) {
             return null;
         }
-        Map<Integer,Integer> hashMap = new HashMap<>();
-        for (int i=0;i<people.length;i++){
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < people.length; i++) {
             int teacher = people[i][0];
             int student = people[i][1];
             // teacher大于student，说明该teacher的徒弟考试超过他
-            if (teacher>student){
-                hashMap.put(teacher,hashMap.getOrDefault(student,0)+1);
+            if (teacher > student) {
+                hashMap.put(teacher, hashMap.getOrDefault(student, 0) + 1);
             }
-            if (!hashMap.containsKey(student)){
-                hashMap.put(student,0);
+            if (!hashMap.containsKey(student)) {
+                hashMap.put(student, 0);
             }
         }
         int[] ans = new int[hashMap.keySet().size()];
         int index = 0;
-        for (int key:hashMap.keySet()){
+        for (int key : hashMap.keySet()) {
             ans[index] = hashMap.get(key);
             index++;
         }
@@ -38,15 +39,15 @@ public class FindStrongPeople {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()){
+        while (sc.hasNext()) {
             int numLen = sc.nextInt();
             int[][] arr = new int[numLen][];
             int i = 0;
             // 放到此处
             sc.nextLine();
-            while(i < numLen){
+            while (i < numLen) {
                 String[] str = sc.nextLine().split(",");
-                arr[i] = new int[]{Integer.parseInt(str[0]),Integer.parseInt(str[1])};
+                arr[i] = new int[]{Integer.parseInt(str[0]), Integer.parseInt(str[1])};
                 i++;
             }
             int[] ans = new FindStrongPeople().getStrongPeople(arr);
