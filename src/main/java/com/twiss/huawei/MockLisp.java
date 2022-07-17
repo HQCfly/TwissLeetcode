bfa45e62008b463a99b2b90da7422e9ceaee4026
HQCfly <2249629867@qq.com>
2022-07-17 22:37:36 +0800

    Add MockLISP

---
+++ src/main/java/com/twiss/huawei/MockLisp.java
@@ -0,0 +1,15 @@
+package com.twiss.huawei;
+
+/**
+ * 仿LISP计算
+ * @Author: Twiss
+ * @Date: 2022/7/17 10:11 下午
+ */
+public class MockLisp {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
