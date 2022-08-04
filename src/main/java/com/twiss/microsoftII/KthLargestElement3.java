d15b281c581677858527a20a954af10ca71b089d
HQCfly <2249629867@qq.com>
2022-08-04 17:52:07 +0800

    modified kth largest element

--- src/main/java/com/twiss/microsoftII/KthLargestElement3.java
+++ src/main/java/com/twiss/microsoftII/KthLargestElement3.java
@@ -1,16 +1,52 @@
 package com.twiss.microsoftII;
 
+import java.util.Random;
+
 /**
  * @Author: Twiss
  * @Date: 2022/7/30 1:54 下午
  */
 public class KthLargestElement3 {
 
-    public int getKthEle(int[] nums){
-        return  0;
+    public int getKthEle(int[] nums,int k){
+        int n = nums.length;
+        return  partition(nums,0,n-1,n-k);
+    }
+
+    private int partition(int[] nums, int left, int right, int index){
+        int pivot = randomPartition(nums,left,right);
+        if (pivot==index){
+            return nums[pivot];
+        }
+        return pivot<index?partition(nums,pivot+1,right,index):
+                partition(nums,left,pivot-1,index);
+    }
+
+    private int randomPartition(int[] nums, int left, int right){
+        int pivot = new Random().nextInt(right-left+1)+left;
+        swap(nums,pivot,right);
+        int l = left-1;
+        int p = nums[right];
+        for (int i=left;i<right;i++){
+            if (nums[i]<=p){
+                l+=1;
+                swap(nums,l,i);
+            }
+        }
+        swap(nums,l+1,right);
+        return l+1;
+    }
+
+    private void swap(int[] nums, int index1, int index2){
+        int tmp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = tmp;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        int ans = new KthLargestElement3().getKthEle(nums,k);
+        System.out.println(ans);
     }
 }
