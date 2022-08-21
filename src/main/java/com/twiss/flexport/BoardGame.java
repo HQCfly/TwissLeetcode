2bf18403d6d7b9a7b78f12a91d8017e89d0a0795
HQCfly <2249629867@qq.com>
2022-08-21 20:35:09 +0800

    Add board game

---
+++ src/main/java/com/twiss/flexport/BoardGame.java
@@ -0,0 +1,149 @@
+package com.twiss.flexport;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 只能网斜前方走
+ * @Author: Twiss
+ * @Date: 2022/8/21 4:30 下午
+ */
+public class BoardGame {
+
+    private char[][] board;
+    private char[][] tmpBoard;
+
+    public BoardGame(){
+
+    }
+
+    public void move(int[] current, int[] target, boolean isNextPlayer){
+        if (canMove(current,target,isNextPlayer)){
+            System.out.println("tmpBoard: "+JSONObject.toJSONString(tmpBoard));
+            board = tmpBoard.clone();
+        }else {
+            System.out.println("Target position couldn't arrival!");
+        }
+    }
+
+    public boolean canMove(int[] current, int[] target, boolean isNextPlayer){
+         tmpBoard = board.clone();
+         int x = current[0];
+         int y = current[1];
+         int targetX = target[0];
+         int targetY = target[1];
+        return moveNextStep(tmpBoard,x,y,targetX,targetY,isNextPlayer);
+    }
+
+    private boolean moveNextStep(char[][] newBoard,
+                                 int x,int y,
+                                 int targetX,int targetY,
+                                 boolean isNextPlayer){
+        // 走出范围
+        // ||白棋：已经走到x坐标小于目标坐标x了说明无法到达
+        // ||黑棋：已经走到x坐标大于目标坐标x了说明无法到达
+        if (x==targetX&&y==targetY){
+            return true;
+        }
+        if (!inAreas(x,y)||
+                (isNextPlayer&&(targetX>x))||
+                (!isNextPlayer&&(targetX<x))){
+
+            return false;
+        }
+
+        boolean ans = false;
+        char tmp = newBoard[x][y];
+        if (isNextPlayer){
+            // 白旗走法
+            if (newBoard[x][y]!='1'){
+                newBoard[x][y] = '2';
+                ans = moveNextStep(newBoard,x-1,y-1,targetX,targetY,isNextPlayer)||
+                        moveNextStep(newBoard,x-1,y+1,targetX,targetY,isNextPlayer);
+                if (!ans){
+                    newBoard[x][y] = tmp;
+                }
+            }
+
+        }else {
+            // 黑旗走法
+            if (newBoard[x][y]!='2'){
+                newBoard[x][y] = '1';
+                ans = moveNextStep(newBoard,x+1,y+1,targetX,targetY,isNextPlayer)||
+                        moveNextStep(newBoard,x+1,y-1,targetX,targetY,isNextPlayer);
+                if (!ans){
+                    System.out.println(x+" "+y);
+                    newBoard[x][y] = tmp;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     *
+     * @param isNextPlayer 那个player的可能要走的steps
+     * @return
+     */
+    public List<int[]> getAllNextStep(boolean isNextPlayer){
+        List<int[]> allNextStep = new ArrayList<>();
+        if (isNextPlayer){
+            getNextStep(board.length-1, board.length-1, allNextStep,isNextPlayer);
+        }else {
+            getNextStep(0,0, allNextStep,isNextPlayer);
+        }
+        return allNextStep;
+    }
+
+    private void getNextStep(int i,int j,List<int[]> steps,boolean isNextPlayer){
+        if (!inAreas(i,j)){
+            return;
+        }
+        if (board[i][j]=='0'){
+            steps.add(new int[]{i,j});
+            return;
+        }
+        if (isNextPlayer){
+            // 白旗走法
+            if (board[i][j]=='2'){
+                getNextStep(i-1,j-1,steps,isNextPlayer);
+                getNextStep(i-1,j+1,steps,isNextPlayer);
+            }
+
+        }else {
+            // 黑旗走法
+            if (board[i][j]=='1'){
+                getNextStep(i+1,j+1,steps,isNextPlayer);
+                getNextStep(i+1,j-1,steps,isNextPlayer);
+            }
+        }
+    }
+
+    private boolean inAreas(int i,int j){
+        return 0<=i&&i< board.length&&
+                0<=j&&j<board.length;
+    }
+
+
+    public static void main(String[] args) {
+        BoardGame bg = new BoardGame();
+        int n = 4;
+        bg.board = new char[][]{
+                {'1','0','0','0'},
+                {'0','1','0','0'},
+                {'0','0','2','0'},
+                {'0','0','0','2'}
+        };
+        List<int[]> black = bg.getAllNextStep(false);
+        System.out.println(JSONObject.toJSONString(black));
+        int[] current = {1,1};
+        int[] target = {3,1};
+        boolean ans = bg.canMove(current,target,false);
+        System.out.println(ans);
+
+        bg.move(current,target,false);
+    }
+}
