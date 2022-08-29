30c87b5d451fd57109d0625881e501ee38d50694
HQCfly <2249629867@qq.com>
2022-08-29 20:58:52 +0800

    code format

--- src/main/java/com/twiss/xioahongshu/Permute.java
+++ src/main/java/com/twiss/xioahongshu/Permute.java
@@ -10,9 +10,9 @@ import java.util.*;
  */
 public class Permute {
 
-    public List<List<Integer>> getResult(int[] nums){
+    public List<List<Integer>> getResult(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums==null||nums.length==0){
+        if (nums == null || nums.length == 0) {
             return ans;
         }
         int len = nums.length;
@@ -20,28 +20,28 @@ public class Permute {
         Arrays.sort(nums);
         Deque<Integer> path = new ArrayDeque<>();
 
-        backtrace(nums,2,0,path,used,ans);
+        backtrace(nums, 2, 0, path, used, ans);
         return ans;
     }
 
     private void backtrace(int[] nums,
-                           int len,int depth,
+                           int len, int depth,
                            Deque<Integer> path,
                            boolean[] used,
-                           List<List<Integer>> res){
-        if (depth==len){
+                           List<List<Integer>> res) {
+        if (depth == len) {
             res.add(new ArrayList<>(path));
             return;
         }
-        for (int i=0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
 //            if (used[i]||(i>0&&nums[i]==nums[i-1]&&!used[i-1])){
 //                continue;
 //            }
-            if (!used[i]){
+            if (!used[i]) {
                 path.addLast(nums[i]);
                 used[i] = true;
 
-                backtrace(nums,len,depth+1,path,used,res);
+                backtrace(nums, len, depth + 1, path, used, res);
                 used[i] = false;
                 path.removeLast();
             }
@@ -56,22 +56,22 @@ public class Permute {
         int k = Integer.parseInt(config[1]);
         int[] nums = new int[n];
         String[] value = sc.nextLine().split(" ");
-        for (int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(value[i]);
         }
         List<List<Integer>> ans = new Permute().getResult(nums);
         System.out.println(JSONObject.toJSONString(ans));
         int res = 0;
-        if (ans.size()>0){
-            for (int i=0;i<ans.size();i++){
+        if (ans.size() > 0) {
+            for (int i = 0; i < ans.size(); i++) {
                 List<Integer> elements = ans.get(i);
                 int tmp = 1;
-                if (elements.size()==2){
-                    for (int j=0;j<2;j++){
-                        tmp*=elements.get(j);
+                if (elements.size() == 2) {
+                    for (int j = 0; j < 2; j++) {
+                        tmp *= elements.get(j);
                     }
                 }
-                if (tmp>=k){
+                if (tmp >= k) {
                     res++;
                 }
             }
