daf14c20bdb2fb65a9bde902fbf238b7e122b017
HQCfly <2249629867@qq.com>
2022-09-12 17:41:30 +0800

    Add delete nth node

---
+++ src/main/java/com/twiss/autodrive/DeleteNthNode.java
@@ -0,0 +1,22 @@
+package com.twiss.autodrive;
+
+/**
+ * 删除倒数第N个节点
+ * @Author: Twiss
+ * @Date: 2022/9/12 5:25 下午
+ */
+public class DeleteNthNode {
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(){}
+    public ListNode(int val){
+        this.val = val;
+    }
+    public ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
